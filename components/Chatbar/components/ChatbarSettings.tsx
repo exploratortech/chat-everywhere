39dc3a664ce7cc8131616d3e2f0545ae87f6ce49
1orzero <laokameng.6@gmail.com>
2023-06-22 10:53:57 +0800

    Add new plan type

--- components/Chatbar/components/ChatbarSettings.tsx
+++ components/Chatbar/components/ChatbarSettings.tsx
@@ -59,6 +59,12 @@ export const ChatbarSettings = () => {
             Pro
           </span>
         );
+      } else if (user.plan === 'edu') {
+        return (
+          <span className="bg-indigo-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">
+            Edu
+          </span>
+        );
       } else {
         return (
           <span className="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400 border border-gray-500">
--- types/user.ts
+++ types/user.ts
@@ -1,5 +1,5 @@
-import { ExportFormatV4 } from "./export";
-import { PluginID } from "./plugin";
+import { ExportFormatV4 } from './export';
+import { PluginID } from './plugin';
 
 export interface User {
   id: string;
@@ -9,25 +9,25 @@ export interface User {
   token: string;
 }
 
-export type SubscriptionPlan = 'free' | 'pro';
+export type SubscriptionPlan = 'free' | 'pro' | 'edu';
 
 export interface UserConversation {
   id: string;
   uid: string;
-  conversations: ExportFormatV4
+  conversations: ExportFormatV4;
 }
 
 export interface UserProfile {
   id: string;
-  plan: "free" | "pro";
+  plan: 'free' | 'pro';
   name: string;
 }
 
 export interface CreditUsage {
   [PluginID.GPT4]: {
     remainingCredits: number;
-  }
+  };
   [PluginID.IMAGE_GEN]: {
     remainingCredits: number;
-  }
+  };
 }
