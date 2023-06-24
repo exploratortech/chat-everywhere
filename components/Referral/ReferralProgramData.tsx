1f5add5fbc5069a0845b08be7978bc179a8c307c
1orzero <laokameng.6@gmail.com>
2023-06-24 11:05:56 +0800

    Update coming soon icon style

--- components/Referral/ReferralProgramData.tsx
+++ components/Referral/ReferralProgramData.tsx
@@ -5,7 +5,7 @@ export default function ReferralProgramData() {
   return (
     <div className="text-center opacity-40 my-4 select-none">
       <h1 className="text-xl">Referral Data</h1>
-      <IconPlanet className="w-[40dvw] h-[40dvw]  mx-auto" />
+      <IconPlanet className="w-[20rem] h-[20rem]  mx-auto" />
       <div>Coming soon...</div>
     </div>
   );
