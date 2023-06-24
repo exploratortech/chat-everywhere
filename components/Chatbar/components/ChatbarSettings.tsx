49d12b332757e538515290d62c39925442606c39
1orzero <laokameng.6@gmail.com>
2023-06-24 11:07:52 +0800

    Update Edu badge light mode style

--- components/Chatbar/components/ChatbarSettings.tsx
+++ components/Chatbar/components/ChatbarSettings.tsx
@@ -71,7 +71,7 @@ export const ChatbarSettings = () => {
         );
       } else if (user.plan === 'edu') {
         return (
-          <span className="bg-indigo-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">
+          <span className="text-xs font-medium mr-2 px-2.5 py-0.5 rounded bg-gray-700 text-green-400 border border-green-400">
             Edu
           </span>
         );
