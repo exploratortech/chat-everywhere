f195291133de293e2e1d46fc1dba4852659586b2
1orzero <laokameng.6@gmail.com>
2023-06-24 12:43:29 +0800

    add cron job test

--- pages/api/cron/refresh-referral-codes.ts
+++ pages/api/cron/refresh-referral-codes.ts
@@ -1,13 +1,25 @@
 import { batchRefreshReferralCodes } from '../../../utils/server/supabase';
 
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 export const config = {
   runtime: 'edge',
 };
 
-// TODO: move to cron job
 const handler = async (req: Request): Promise<Response> => {
   try {
-    await batchRefreshReferralCodes();
+    console.log({
+      twTimeNow: dayjs
+        .tz(new Date(), 'Asia/Taipei')
+        .format('YYYY-MM-DD HH:mm:ss'),
+    });
+    // TODO: uncomment this when ready
+    // await batchRefreshReferralCodes();
     return new Response(JSON.stringify({ success: true }), { status: 200 });
   } catch (error) {
     console.error(error);
