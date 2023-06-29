ec4b2091f93bbb6e0ca1bfe2b89c71386db9b00a
williamtriinh <williamtrinh2039@gmail.com>
2023-06-29 11:56:45 -0400

    Fix hover effect not working on conversations

--- components/Chatbar/components/Conversation.tsx
+++ components/Chatbar/components/Conversation.tsx
@@ -130,11 +130,11 @@ export const ConversationComponent = ({ conversation }: Props) => {
         </div>
       ) : (
         <button
-          className={`flex w-full cursor-pointer items-center gap-3 rounded-lg p-3 text-sm transition-colors duration-200 hover:bg-[#343541]/90 ${
+          className={`flex w-full cursor-pointer items-center gap-3 rounded-lg p-3 text-sm transition-colors duration-200 hover:!bg-[#343541]/90 ${
             messageIsStreaming ? 'disabled:cursor-not-allowed' : ''
           } ${
             selectedConversation?.id === conversation.id
-              ? 'bg-[#343541]/90'
+              ? '!bg-[#343541]/90'
               : ''
           }`}
           onClick={() => handleSelectConversation(conversation)}
