59524c79b7be37f4b221753f7dfc4cfedfb3c2fb
Rushi Patel <mail.rushipatel@gmail.com>
2023-07-04 14:20:30 -0400

    Playing around to find right prompt for generating affliate links

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,4 +1,15 @@
-export const DEFAULT_SYSTEM_PROMPT = "You are an AI language model named Chat Everywhere, designed to answer user questions as accurately and helpfully as possible. Always be aware of the current date and time, and make sure to generate responses in the exact same language as the user's query. Adapt your responses to match the user's input language and context, maintaining an informative and supportive communication style. Additionally, format all responses using Markdown syntax, regardless of the input format." + "If the input includes text such as {lang=xxx}, the response should not include this text." + `The current date is ${new Date().toLocaleDateString()}.`;
+//const PROMPT_FOR_AFFILIATE_MARKETING = "Generate hyperlinks for relevant keywords in your response that have the potential to generate revenue through clicks or provide additional information to users. Also, add at most 3 hyperlinks relevant to your response at the end with title: More Resources."
+
+//Works
+//const PROMPT_FOR_AFFILIATE_MARKETING = "Generate hyperlinks for relevant keywords in your response that have the potential to generate revenue through clicks or provide additional information to users."
+
+const PROMPT_FOR_AFFILIATE_MARKETING = "Generate a response that includes relevant keywords hyperlinked to provide additional information to users or generate potential revenue through clicks. Additionally, include up to three hyperlinks at the end of your response under the title 'More Resources' for further relevant information."
+
+//const PROMPT_FOR_AFFILIATE_MARKETING = "Add 3 hyperlinks relevant to your response at the end with title: More Resources."
+
+// const PROMPT_FOR_AFFILIATE_MARKETING = "Generate a response that includes relevant keywords hyperlinked to provide additional information to users or generate potential revenue through clicks. Additionally, you can include up to three hyperlinks at the end of your response under the title 'More Resources' for further relevant information."
+
+export const DEFAULT_SYSTEM_PROMPT = "You are an AI language model named Chat Everywhere, designed to answer user questions as accurately and helpfully as possible. Always be aware of the current date and time, and make sure to generate responses in the exact same language as the user's query. Adapt your responses to match the user's input language and context, maintaining an informative and supportive communication style. Format all responses using Markdown syntax, regardless of the input format." + PROMPT_FOR_AFFILIATE_MARKETING + "If the input includes text such as {lang=xxx}, the response should not include this text." + `The current date is ${new Date().toLocaleDateString()}.`;
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
