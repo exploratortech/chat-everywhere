89f7098fab499b85e5ea40fb8d5167bf5e67e613
1orzero <laokameng.6@gmail.com>
2023-07-05 12:12:18 +0800

    Refactor stripe webhook, update error handling

--- pages/api/webhooks/stripe.ts
+++ pages/api/webhooks/stripe.ts
@@ -1,198 +1,88 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { sendReportForStripeWebhookError } from '@/utils/server/resend';
-import updateUserAccount from '@/utils/server/stripe/updateUserAccount';
-import {
-  getAdminSupabaseClient,
-  userProfileQuery,
-} from '@/utils/server/supabase';
+import handleCheckoutSessionCompleted from '@/utils/server/stripe/handleCheckoutSessionCompleted';
+import handleCustomerSubscriptionDeleted from '@/utils/server/stripe/handleCustomerSubscriptionDeleted';
+import handleCustomerSubscriptionUpdated from '@/utils/server/stripe/handleCustomerSubscriptionUpdated';
 
-import { SubscriptionPlan } from './../../../types/user';
+import { UserProfile } from './../../../types/user';
 
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
 import getRawBody from 'raw-body';
 import Stripe from 'stripe';
-
-dayjs.extend(utc);
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2022-11-15',
 });
-const ONE_TIME_PRO_PLAN_FOR_1_MONTH =
-  process.env.STRIPE_PLAN_CODE_ONE_TIME_PRO_PLAN_FOR_1_MONTH;
-const MONTHLY_PRO_PLAN_SUBSCRIPTION =
-  process.env.STRIPE_PLAN_CODE_MONTHLY_PRO_PLAN_SUBSCRIPTION;
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-    const sig = req.headers['stripe-signature'] as string;
-    let event: Stripe.Event;
-    let stripeSubscriptionId: string | null = null;
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+    return;
+  }
+  const sig = req.headers['stripe-signature'] as string;
+  let event: Stripe.Event;
 
-    // Read the raw body
-    const rawBody = await getRawBody(req);
+  // Read the raw body
+  const rawBody = await getRawBody(req);
 
-    try {
-      event = stripe.webhooks.constructEvent(
-        rawBody,
-        sig,
-        process.env.STRIPE_WEBHOOK_SECRET!,
-      );
-    } catch (err) {
-      console.error(
-        `Webhook signature verification failed.`,
-        (err as any).message,
-      );
-      return res.status(400).send(`Webhook signature verification failed.`);
-    }
+  try {
+    event = stripe.webhooks.constructEvent(
+      rawBody,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET!,
+    );
+  } catch (err) {
+    console.error(
+      `Webhook signature verification failed.`,
+      (err as any).message,
+    );
+    return res.status(400).send(`Webhook signature verification failed.`);
+  }
 
-    // One time payment / Initial Monthly Pro Plan Subscription
-    if (event.type === 'checkout.session.completed') {
-      const session = event.data.object as Stripe.Checkout.Session;
-      const userId = session.client_reference_id;
-      const email = session.customer_details?.email;
-
-      const planCode = session.metadata?.plan_code;
-      const planGivingWeeks = session.metadata?.plan_giving_weeks;
-      stripeSubscriptionId = session.subscription as string;
-
-      if (!planCode && !planGivingWeeks) {
-        await sendReportForStripeWebhookError(
-          'no plan code or plan giving weeks from Stripe webhook',
-          event,
+  try {
+    switch (event.type) {
+      case 'checkout.session.completed':
+        // One time payment / Initial Monthly Pro Plan Subscription
+        await handleCheckoutSessionCompleted(
+          event.data.object as Stripe.Checkout.Session,
         );
-        throw new Error(
-          'no plan code or plan giving weeks from Stripe webhook',
+        break;
+      case 'customer.subscription.updated':
+        // Monthly Pro Plan Subscription recurring payment
+        await handleCustomerSubscriptionUpdated(
+          event.data.object as Stripe.Subscription,
         );
-      }
-
-      if (!email) {
-        await sendReportForStripeWebhookError(
-          'missing Email from Stripe webhook',
-          event,
+        break;
+      case 'customer.subscription.deleted':
+        // Monthly Pro Plan Subscription removal
+        await handleCustomerSubscriptionDeleted(
+          event.data.object as Stripe.Subscription,
         );
-        throw new Error('missing Email from Stripe webhook');
-      }
-
-      const sinceDate = dayjs.unix(session.created).utc().toDate();
-      const proPlanExpirationDate = await (async () => {
-        // Takes plan_giving_weeks priority over plan_code
-        if (planGivingWeeks && typeof planGivingWeeks === 'string') {
-          // Get users' pro expiration date
-          const supabase = getAdminSupabaseClient();
-          const user = await userProfileQuery({
-            client: supabase,
-            email,
-          });
-          const userProPlanExpirationDate = user?.proPlanExpirationDate;
-          if (userProPlanExpirationDate) {
-            // when user bought one-time pro plan previously or user has referral trial
-            return dayjs(userProPlanExpirationDate)
-              .add(+planGivingWeeks, 'week')
-              .toDate();
-          } else if (user.plan === 'pro' && !user.proPlanExpirationDate) {
-            // when user is pro monthly subscriber
-            await sendReportForStripeWebhookError(
-              'Monthly Pro subscriber bought one-time pro plan',
-              event,
-              user,
-            );
-
-            throw new Error(
-              'Monthly Pro subscriber bought one-time pro plan, should not happen',
-            );
-          } else {
-            // when user is not pro yet
-            return dayjs(sinceDate).add(+planGivingWeeks, 'week').toDate();
-          }
-        } else if (planCode === ONE_TIME_PRO_PLAN_FOR_1_MONTH) {
-          // Only store expiration for one month plan
-          return dayjs(sinceDate).add(1, 'month').toDate();
-        } else {
-          return undefined;
-        }
-      })();
-
-      // Update user account by User id
-      if (userId) {
-        await updateUserAccount({
-          upgrade: true,
-          userId,
-          stripeSubscriptionId,
-          proPlanExpirationDate: proPlanExpirationDate,
-        });
-      } else {
-        // Update user account by Email
-        await updateUserAccount({
-          upgrade: true,
-          email: email!,
-          stripeSubscriptionId,
-          proPlanExpirationDate: proPlanExpirationDate,
-        });
-      }
-    }
-
-    // Monthly Pro Plan Subscription recurring payment
-    if (event.type === 'customer.subscription.updated') {
-      const session = event.data.object as Stripe.Subscription;
-      stripeSubscriptionId = session.id;
-
-      if (!stripeSubscriptionId) {
-        await sendReportForStripeWebhookError(
-          'Subscription id not found from Stripe webhook',
-          event,
-        );
-        throw new Error('Subscription id not found from Stripe webhook');
-      }
-
-      if (!session.cancel_at) {
-        return res.json({ received: true });
-      }
-
-      const cancelAtDate = dayjs.unix(session.cancel_at!).utc().toDate();
-      const today = dayjs().utc().toDate();
-
-      if (cancelAtDate < today) {
-        // Downgrade to free plan
-        await updateUserAccount({
-          upgrade: false,
-          stripeSubscriptionId,
-        });
-      } else {
-        // Monthly Pro Plan Subscription recurring payment, extend expiration date
-        await updateUserAccount({
-          upgrade: true,
-          stripeSubscriptionId,
-          proPlanExpirationDate: undefined,
-        });
-      }
-    }
-
-    // Monthly Pro Plan Subscription removal
-    if (event.type === 'customer.subscription.deleted') {
-      const session = event.data.object as Stripe.Subscription;
-      stripeSubscriptionId = session.id;
-
-      if (!stripeSubscriptionId) {
-        await sendReportForStripeWebhookError(
-          'Subscription id not found from Stripe webhook',
-          event,
-        );
-        throw new Error('Subscription id not found from Stripe webhook');
-      }
-
-      await updateUserAccount({
-        upgrade: false,
-        stripeSubscriptionId,
-      });
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
     }
 
     // Return a response to acknowledge receipt of the event
     res.json({ received: true });
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+  } catch (error) {
+    if (error instanceof Error) {
+      const user =
+        error.cause && typeof error.cause === 'object' && 'user' in error.cause
+          ? (error.cause.user as UserProfile)
+          : undefined;
+      sendReportForStripeWebhookError(error.message, event, user)
+        .then(() => {
+          console.log('webhook catch errors, send report success');
+        })
+        .catch((error) => {
+          console.error(error);
+          throw error;
+        });
+      return res.json({ received: true, error: error.message });
+    }
+    throw error;
   }
 };
 
