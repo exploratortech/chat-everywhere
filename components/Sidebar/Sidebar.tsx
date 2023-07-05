c047bfb4d9e87f28f0e7dbcf0b79c31f6ba7dff3
williamtriinh <williamtrinh2039@gmail.com>
2023-07-05 12:21:57 -0400

    Fix navbar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -51,14 +51,14 @@ const Sidebar = <T,>({
   return (
     <div
       className={`
-        ${ isOpen ? 'w-[260px]' : 'w-0' }
-        ${ side === 'left' ? 'tablet:left-0' : 'tablet:right-0' }
-        transition-all ease-linear relative box-content bg-[#202123] tablet:h-[calc(100%-48px)] tablet:fixed tablet:z-10
+        ${isOpen ? 'w-[260px]' : 'w-0'}
+        ${side === 'left' ? 'tablet:left-0' : 'tablet:right-0'}
+        transition-all ease-linear relative h-full box-content bg-[#202123] tablet:fixed tablet:z-10
       `}
     >
       <div
         className={`
-          ${ isOpen ? 'tablet:visible !bg-[#202123]/90' : '' }
+          ${isOpen ? 'tablet:visible !bg-[#202123]/90' : ''}
           fixed invisible left-0 w-full h-full bg-transparent transition-all ease-linear -z-10
         `}
         onClick={toggleOpen}
@@ -66,19 +66,20 @@ const Sidebar = <T,>({
       <div
         className={`
           absolute block tablet:hidden z-50
-          ${ side === 'left' ? 'right-0 translate-x-full' : 'left-0 -translate-x-full' }
+          ${
+            side === 'left'
+              ? 'right-0 translate-x-full'
+              : 'left-0 -translate-x-full'
+          }
         `}
       >
-        <SidebarToggleButton
-          onClick={toggleOpen}
-          isOpen={isOpen}
-        />
+        <SidebarToggleButton onClick={toggleOpen} isOpen={isOpen} />
       </div>
       <div
         className={`
-          ${ side === 'left' && !isOpen ? '-translate-x-full' : '' }
-          ${ side === 'right' && !isOpen ? 'translate-x-full' : '' }
-          transition-all ease-linear flex w-[260px] h-full flex-none flex-col p-2 space-y-2 text-[14px]
+          ${side === 'left' && !isOpen ? '-translate-x-full' : ''}
+          ${side === 'right' && !isOpen ? 'translate-x-full' : ''}
+          z-10 top-0 ${side}-0 flex transition-all ease-linear w-[260px] h-full flex-none flex-col p-2 space-y-2 text-[14px]
         `}
       >
         <div className="flex items-center">
