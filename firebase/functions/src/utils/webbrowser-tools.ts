7de49cfc03cc56243111f106c971380fe54a2a5a
1orzero <laokameng.6@gmail.com>
2023-07-07 12:37:46 +0800

    remove unused code

--- firebase/functions/src/utils/webbrowser-tools.ts
+++ firebase/functions/src/utils/webbrowser-tools.ts
@@ -12,63 +12,50 @@ export async function getMDContentOfArticle(page: Page): Promise<string> {
 async function getContentOfArticle(page: Page) {
   const articleHTML = await page.evaluate(() => {
     function getContainer() {
-      let selectedContainer;
-
-      if (document.head.querySelector("meta[name='articleBody'")) {
-        selectedContainer = document.createElement("div");
-        selectedContainer.innerHTML = sanitize(
-          document.head
-            .querySelector("meta[name='articleBody'")
-            ?.getAttribute("content") ?? "",
-        );
-      } else {
-        const numWordsOnPage =
+      const numWordsOnPage =
           document.body.innerText.match(/\S+/g)?.length ?? 0;
-        let ps = document.body.querySelectorAll("p");
+      let ps = document.body.querySelectorAll("p");
 
-        // Find the paragraphs with the most words in it
-        let pWithMostWords = document.body;
-        let highestWordCount = 0;
+      // Find the paragraphs with the most words in it
+      let pWithMostWords = document.body;
+      let highestWordCount = 0;
 
-        if (ps.length === 0) {
-          ps = document.body.querySelectorAll("div");
-        }
+      if (ps.length === 0) {
+        ps = document.body.querySelectorAll("div");
+      }
 
-        ps.forEach((p) => {
-          if (p.offsetHeight !== 0) {
-            //  Make sure it's visible on the regular page
-            const myInnerText = p.innerText.match(/\S+/g);
-            if (myInnerText) {
-              const wordCount = myInnerText.length;
-              if (wordCount > highestWordCount) {
-                highestWordCount = wordCount;
-                pWithMostWords = p;
-              }
+      ps.forEach((p) => {
+        if (p.offsetHeight !== 0) {
+          //  Make sure it's visible on the regular page
+          const myInnerText = p.innerText.match(/\S+/g);
+          if (myInnerText) {
+            const wordCount = myInnerText.length;
+            if (wordCount > highestWordCount) {
+              highestWordCount = wordCount;
+              pWithMostWords = p;
             }
           }
-        });
+        }
+      });
 
-        // Keep selecting more generally until over 2/5th of the
-        // words on the page
-        // have been selected
-        selectedContainer = pWithMostWords;
-        let wordCountSelected = highestWordCount;
+      let selectedContainer:HTMLElement | null = pWithMostWords;
+      let wordCountSelected = highestWordCount;
 
-        while (
-          wordCountSelected / numWordsOnPage < 0.4 &&
+      while (
+        wordCountSelected / numWordsOnPage < 0.4 &&
           selectedContainer != document.body &&
           selectedContainer.parentElement?.innerText
-        ) {
-          selectedContainer = selectedContainer.parentElement;
-          wordCountSelected =
+      ) {
+        selectedContainer = selectedContainer.parentElement;
+        wordCountSelected =
             selectedContainer.innerText.match(/\S+/g)?.length ?? 0;
-        }
-
-        // Make sure a single p tag is not selected
-        if (selectedContainer.tagName === "P") {
-          selectedContainer = selectedContainer.parentElement;
-        }
       }
+
+      // Make sure a single p tag is not selected
+      if (selectedContainer.tagName === "P") {
+        selectedContainer = selectedContainer.parentElement;
+      }
+
 
       return selectedContainer;
     }
