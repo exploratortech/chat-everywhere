ced79e3dd79eceaec5e759f5526540d29d77c212
1orzero <laokameng.6@gmail.com>
2023-07-07 11:50:57 +0800

    update web-summary endpoint

--- pages/api/web-summary.ts
+++ pages/api/web-summary.ts
@@ -1,39 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { PromptTemplate } from "langchain/prompts";
-import { OpenAI } from "langchain/llms";
-import { OpenAIModelID } from '@/types/openai';
-import { LLMChain } from 'langchain';
-const template  =
-  `Output a nice format of the content in Markdown based on the input:
-  {input} `;
-const prompt = new PromptTemplate({
-  template: template,
-  inputVariables: ["input"],
-});
-
+  
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   const { browserQuery } = req.query;
   const url = browserQuery as string;
-  const llm = new OpenAI({
-    openAIApiKey: process.env.OPENAI_API_KEY,
-    modelName: OpenAIModelID.GPT_3_5_16K,
-  });
-  const chain = new LLMChain({ llm, prompt });
+  
   try{
 
-    const endpointHost = "https://asia-east1-chateverywhere-3cf98.cloudfunctions.net/webContent"
+    const endpointHost = process.env.WEB_CONTENT_FUNCTION_ENDPOINT
+    if (!endpointHost) throw new Error("WEB_CONTENT_FUNCTION_ENDPOINT is not defined")
+
     const endpoint = new URL(endpointHost)
     endpoint.searchParams.append("url", url)
     const webContentResponse=  await fetch(endpoint.toString())
     const{content: webMdContent}= await webContentResponse.json()
-    const response = await chain.call({ input: webMdContent});
-
-  
-    res.status(200).json({
-      content: response,
+        res.status(200).json({
+      content: webMdContent 
     });
   } catch (error) {
     console.error(error);
