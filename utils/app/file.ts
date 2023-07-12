9b4ca89bf719bbbe8c83b760bb30215d8a42f1ea
williamtriinh <williamtrinh2039@gmail.com>
2023-07-12 17:38:01 -0400

    Stub the writeToFile function

--- utils/app/file.ts
+++ utils/app/file.ts
@@ -3,6 +3,7 @@ export const readFromFile = (filename: string): string => {
   return 'Yearly Goals:\n1. Increase revenue by 10%\n2. Expand into new markets\n\nMonthly Goals:\n1. Achieve sales target\n2. Improve customer satisfaction\n\nWeekly Goals:\n1. Complete project tasks\n2. Conduct team meetings';
 };
 
-export const writeToFile = (filename: string, content: string): void => {
-
+export const writeToFile = (filename: string, content: string): string => {
+  console.warn('TODO: Implement "writeToFile()"');
+  return content;
 };
