20e2e2c37614e518d4ada51d619b75d49362940f
Jack wu <jackwu833016@gmail.com>
2023-07-12 21:39:26 -0400

    First attempt in PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const { i18n } = require('./next-i18next.config');
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
@@ -25,4 +30,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -683,7 +683,6 @@ const Home = () => {
           name="viewport"
           content="height=device-height ,width=device-width, initial-scale=1, user-scalable=no"
         />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       {selectedConversation && (
         <main
