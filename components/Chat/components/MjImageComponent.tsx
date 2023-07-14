7a74acb1cd2d47ceb6cc45acccc3e9d0371e57d0
1orzero <laokameng.6@gmail.com>
2023-07-14 10:31:09 +0800

    Update MJ image component style

--- components/Chat/components/MjImageComponent.tsx
+++ components/Chat/components/MjImageComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import { useQuery } from 'react-query';
 
 import HomeContext from '@/pages/api/home/home.context';
@@ -8,6 +8,7 @@ interface MjImageComponentProps {
   buttons: string[];
   buttonMessageId: string;
 }
+type Command = 'upscale' | '🔍 Zoom Out 2x' | '🔍 Zoom Out 1.5x';
 
 export default function MjImageComponent({
   src,
@@ -42,26 +43,62 @@ export default function MjImageComponent({
   const { data, isLoading, error } = useQuery('upscale-image', upscaleImage, {
     enabled: isUpscaled,
   });
+
+  const availableCommands = useMemo(() => {
+    let commands: Command[] = [];
+    // if buttons list has a element with text like "U{number}" then it is a upscale command
+    const upscaleCommand = buttons.find((button) => button.match(/^U\d+$/));
+    const zoomOutCommands = buttons.filter((button) =>
+      button.match(/^🔍 Zoom Out/),
+    );
+    if (upscaleCommand) {
+      commands.push('upscale');
+    }
+    if (zoomOutCommands.length > 0) {
+      commands.push(...(zoomOutCommands as Command[]));
+    }
+
+    return commands;
+  }, [buttons]);
+
   const upscaleImageButtonOnClick = async () => {
     setIsUpscaled(true);
   };
-  useEffect(() => {
-    console.log({
-      data,
-    });
-  }, [data]);
 
   return (
-    // eslint-disable-next-line @next/next/no-img-element
-    <img
-      onClick={upscaleImageButtonOnClick}
-      src={src}
-      alt=""
-      className={`${
-        user ? 'hover:scale-125' : ''
-      } w-full m-0 transition-all duration-500 cursor-pointer hover:drop-shadow-lg`}
-      data-ai-image-selection={JSON.stringify(buttons)}
-      data-ai-image-message-id={buttonMessageId}
-    />
+    <div className="group">
+      <div className={`group/image relative hover:z-10`}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={src}
+          alt=""
+          className={`${
+            user ? `group-hover/image:scale-110` : ''
+          } w-full m-0 transition-all duration-500 `}
+          data-ai-image-selection={JSON.stringify(buttons)}
+          data-ai-image-message-id={buttonMessageId}
+        />
+        <div
+          className={`${
+            user ? `group-hover/image:scale-110` : ''
+          } group-hover/image:drop-shadow-lg group-hover/image:bg-black/75 transition-all duration-500 absolute top-0 left-0 w-full h-full`}
+        >
+          {/*  Button selections  */}
+          <div className="hidden group-hover/image:flex flex-col justify-center items-center h-full">
+            {availableCommands.map((command, index) => {
+              return (
+                <button
+                  key={`${command}-${index}`}
+                  className="cursor-pointer select-none border border-white text-white font-bold py-2 px-4"
+                  onClick={upscaleImageButtonOnClick}
+                >
+                  {command}
+                </button>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
