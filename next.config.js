858d6f3fcb7b70f64084f42d185e6db4019a12f2
williamtriinh <williamtrinh2039@gmail.com>
2023-07-17 15:01:50 -0400

    Connect to GlitchTip

--- next.config.js
+++ next.config.js
@@ -20,3 +20,45 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+
+// Injected content via Sentry wizard below
+
+const { withSentryConfig } = require("@sentry/nextjs");
+
+module.exports = withSentryConfig(
+  module.exports,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+
+    org: "explorator-labs",
+    project: "chat-everywhere",
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Setting this option to false fixes issues when running the app
+    // https://github.com/getsentry/sentry-javascript/issues/5864
+    autoInstrumentServerFunctions: false,
+  }
+);
---
+++ sentry.client.config.ts
@@ -0,0 +1,2 @@
+// Opt out of Sentry SDK in client side
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/?original_referrer=https%3A%2F%2Fsentry.io%2F#opt-out-of-sentry-sdk-bundling-in-client-or-server-side
