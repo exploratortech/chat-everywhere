7e9d62c13b4f1748ee6a5757130a23d15cc32484
1orzero <laokameng.6@gmail.com>
2023-07-20 12:14:17 +0800

    improve toggle behavior for the new progress bar

--- components/Chat/components/MjImageProgress.tsx
+++ components/Chat/components/MjImageProgress.tsx
@@ -21,50 +21,44 @@ export default function MjImageProgress({
   percentage,
 }: MjImageProgressProps) {
   return (
-    <div className="group relative my-4" tabIndex={0}>
-      <label
-        htmlFor="toggle-progress"
-        className={`${state === 'loading' ? 'bg-white' : ''} ${
-          state === 'completed' ? 'bg-green-200' : ''
-        } ${
-          state === 'error' ? 'bg-red-200' : ''
-        } block text-black rounded-lg cursor-pointer`}
-      >
-        <div className="p-2 flex gap-2 items-center justify-between">
-          <div className="flex gap-2 items-center flex-grow font-bold">
-            {state === 'loading' && <Spinner size="16px" />}
-            {state === 'error' && <IconX size="16px" />}
-            {state === 'completed' && <IconCheck size="16px" />}
-            {state === 'loading' && 'Loading...'}
-            {state === 'loading' && percentage && (
-              <ProgressBar
-                completed={+percentage}
-                className="basis-[50%]"
-                bgColor="#70cc60"
-                height="15px"
-                labelSize="12px"
-              />
-            )}
+    <details
+      className={`${state === 'loading' ? 'bg-white disabled' : ''} ${
+        state === 'completed' ? 'bg-green-200' : ''
+      } ${
+        state === 'error' ? 'bg-red-200' : ''
+      } relative my-4 block text-black rounded-lg`}
+      open={state === 'loading'}
+    >
+      <summary className="cursor-pointer p-2 flex gap-2 items-center justify-between">
+        <div className="flex gap-2 items-center flex-grow font-bold">
+          {state === 'loading' && <Spinner size="16px" />}
 
-            {state !== 'loading' &&
-              state.charAt(0).toUpperCase() + state.slice(1)}
-          </div>
-
-          {state !== 'loading' && (
-            <>
-              <IconArrowDown className="toggle-arrow-down h-3 group-focus:hidden" />
-              <IconArrowUp className="toggle-arrow-up h-3 hidden group-focus:block" />
-            </>
+          {state === 'loading' && 'Loading...'}
+          {state === 'loading' && percentage && (
+            <ProgressBar
+              completed={+percentage}
+              className="basis-[50%]"
+              bgColor="#70cc60"
+              height="15px"
+              labelSize="12px"
+            />
           )}
+
+          {state === 'error' && <IconX size="16px" />}
+          {state === 'completed' && <IconCheck size="16px" />}
+          {state !== 'loading' &&
+            state.charAt(0).toUpperCase() + state.slice(1)}
         </div>
-        {state === 'loading' ? (
-          <div className="panel p-2 max-h-full">{content}</div>
-        ) : (
-          <div className="panel group-focus:p-2 max-h-0 transition-all duration-200 overflow-hidden group-focus:max-h-full">
-            {content}
-          </div>
+
+        {state !== 'loading' && (
+          <>
+            <IconArrowUp className="toggle-arrow h-3" />
+          </>
         )}
-      </label>
-    </div>
+      </summary>
+      <main>
+        <div className="panel p-2 max-h-full">{content}</div>
+      </main>
+    </details>
   );
 }
