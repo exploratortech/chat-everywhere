2eb1b9333a5807b5875576a75c0783bfe4866231
williamtriinh <williamtrinh2039@gmail.com>
2023-07-25 09:52:12 -0400

    Create conversations and instant message app users tables

---
+++ supabase/migrations/20230724222753_create_conversations_table.sql
@@ -0,0 +1,21 @@
+drop function if exists "public"."get_referees_profile_by_referrer_id"(referrer uuid);
+
+create table "public"."conversations" (
+    "id" uuid not null,
+    "created_at" timestamp with time zone default now(),
+    "user_id" uuid not null,
+    "content" jsonb not null default '{}'::jsonb
+);
+
+
+alter table "public"."conversations" enable row level security;
+
+CREATE UNIQUE INDEX conversations_pkey ON public.conversations USING btree (id);
+
+alter table "public"."conversations" add constraint "conversations_pkey" PRIMARY KEY using index "conversations_pkey";
+
+alter table "public"."conversations" add constraint "conversations_user_id_fkey" FOREIGN KEY (user_id) REFERENCES profiles(id) ON DELETE CASCADE not valid;
+
+alter table "public"."conversations" validate constraint "conversations_user_id_fkey";
+
+
