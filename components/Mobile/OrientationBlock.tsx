bd571136a42a274d31e6251eef6f04d757eb4878
1orzero <laokameng.6@gmail.com>
2023-08-08 14:50:39 +0800

    Add translation

--- components/Mobile/OrientationBlock.tsx
+++ components/Mobile/OrientationBlock.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import useDeviceDetect from '@/hooks/useDeviceDetect';
 import useOrientation from '@/hooks/useOrientation';
@@ -20,19 +21,21 @@ const OrientationBlock: React.FC<OrientationBlockProps> = ({ children }) => {
     ? 'block'
     : 'hidden';
 
+  const { t } = useTranslation('common');
   return (
     <>
       <div
         className={`relative inset-0 bg-white dark:bg-[#343541] dark:text-white w-[100dvw] h-[100dvh] text-center px-4 py-8 ${showBlocker}`}
       >
         <h1 className="text-2xl font-bold mb-4">
-          We appreciate your use of our application.
+          {t('We appreciate your use of our application.')}
         </h1>
         <p className="mb-4">
-          Currently, our app is optimized to work in portrait mode and
-          unfortunately, we do not support landscape mode at this moment.
+          {t(
+            'Currently, our app is optimized to work in portrait mode and unfortunately, we do not support landscape mode at this moment.',
+          )}
         </p>
-        <p>Thank you for your understanding and patience.</p>
+        <p>{t('Thank you for your understanding and patience.')}</p>
       </div>
       <div className={`${showContent}`}>{children}</div>
     </>
