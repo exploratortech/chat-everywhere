4d2cd3c2cdf3365827583281808b4241d235478f
1orzero <laokameng.6@gmail.com>
2023-08-08 14:58:55 +0800

    add test trigger

--- hooks/useOrientation.ts
+++ hooks/useOrientation.ts
@@ -3,13 +3,13 @@ import { useEffect, useState } from 'react';
 const useOrientation = (): boolean | null => {
   const [landscape, setLandscape] = useState<boolean | null>(null);
 
-  const calculateOrientation = (): void => {
-    setLandscape(
-      typeof window !== 'undefined' && window.innerWidth > window.innerHeight,
-    );
-  };
-
   useEffect(() => {
+    const calculateOrientation = (): void => {
+      alert('trigger calculateOrientation ');
+      setLandscape(
+        typeof window !== 'undefined' && window.innerWidth > window.innerHeight,
+      );
+    };
     if (typeof window !== 'undefined') {
       calculateOrientation();
       window.addEventListener('resize', calculateOrientation);
