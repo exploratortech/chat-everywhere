641104c9696f0a1417637afa807369d40574e6c4
1orzero <laokameng.6@gmail.com>
2023-08-09 12:37:26 +0800

    Fix animation always goes to the same direction & remove comments

--- components/Chat/components/Carousel.tsx
+++ components/Chat/components/Carousel.tsx
@@ -11,26 +11,8 @@ const Carousel: React.FC<CarouselProps> = ({ children }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [direction, setDirection] = useState<'left' | 'right'>('left');
 
-  // const nextSlide = () => {
-  //   setAnimation('slide-out-left');
-  //   const newIndex = currentIndex + 1;
-  //   setTimeout(() => {
-  //     setCurrentIndex(newIndex >= children.length ? 0 : newIndex);
-  //     setAnimation('slide-in-right');
-  //   }, 500);
-  // };
-
-  // const prevSlide = () => {
-  //   setAnimation('slide-out-right');
-  //   const newIndex = currentIndex - 1;
-  //   setTimeout(() => {
-  //     setCurrentIndex(newIndex < 0 ? children.length - 1 : newIndex);
-  //     setAnimation('slide-in-left');
-  //   }, 500);
-  // };
-
   const nextSlide = () => {
-    setDirection('left');
+    setDirection('right');
     const newIndex = currentIndex + 1;
     setCurrentIndex(newIndex >= children.length ? 0 : newIndex);
   };
@@ -55,70 +37,7 @@ const Carousel: React.FC<CarouselProps> = ({ children }) => {
         <AnimatedSlide direction={direction}>
           {currentIndexChildren}
         </AnimatedSlide>
-        {/* <div className={`${animation}`}>
-          <style jsx>{`
-            .slide-in-right {
-              animation: slide-in-right 0.5s forwards;
-            }
 
-            .slide-out-left {
-              animation: slide-out-left 0.5s forwards;
-            }
-
-            .slide-in-left {
-              animation: slide-in-left 0.5s forwards;
-            }
-
-            .slide-out-right {
-              animation: slide-out-right 0.5s forwards;
-            }
-
-            @keyframes slide-in-right {
-              0% {
-                transform: translateX(100%);
-                opacity: 0;
-              }
-              100% {
-                transform: translateX(0);
-                opacity: 1;
-              }
-            }
-
-            @keyframes slide-out-left {
-              0% {
-                transform: translateX(0);
-                opacity: 1;
-              }
-              100% {
-                transform: translateX(-100%);
-                opacity: 0;
-              }
-            }
-
-            @keyframes slide-in-left {
-              0% {
-                transform: translateX(-100%);
-                opacity: 0;
-              }
-              100% {
-                transform: translateX(0);
-                opacity: 1;
-              }
-            }
-
-            @keyframes slide-out-right {
-              0% {
-                transform: translateX(0);
-                opacity: 1;
-              }
-              100% {
-                transform: translateX(100%);
-                opacity: 0;
-              }
-            }
-          `}</style>
-          {currentIndexChildren}
-        </div> */}
         <button
           onClick={prevSlide}
           className="absolute left-[-4rem] mobile:left-[-3rem] top-[50%] translate-y-[-50%] p-4 cursor-pointer text-white"
