e860fb6b2b95d75c9e2c3de1e5f47305a06639df
1orzero <laokameng.6@gmail.com>
2023-08-16 21:25:30 +0800

    User can drag and drop image to (Image to prompt area)

--- components/ImageToPromptUpload/ImageUpload.tsx
+++ components/ImageToPromptUpload/ImageUpload.tsx
@@ -1,38 +1,18 @@
-import { useSupabaseClient } from '@supabase/auth-helpers-react';
-import { IconCirclePlus } from '@tabler/icons-react';
 import React, { useCallback, useContext, useMemo, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 
-import {
-  saveConversation,
-  saveConversations,
-  updateConversationLastUpdatedAtTimeStamp,
-} from '@/utils/app/conversation';
-import {
-  removeRedundantTempHtmlString,
-  removeTempHtmlString,
-} from '@/utils/app/htmlStringHandler';
-import { getAvailableSpeechSourceLanguages } from '@/utils/app/i18n';
 import { handleImageToPromptSend } from '@/utils/app/image-to-prompt';
-import { saveOutputLanguage } from '@/utils/app/outputLanguage';
-import { saveSpeechRecognitionLanguage } from '@/utils/app/speechRecognitionLanguage.ts';
-import { removeSecondLastLine } from '@/utils/app/ui';
-
-import { Conversation, Message } from '@/types/chat';
-import { PluginID } from '@/types/plugin';
 
 import HomeContext from '@/pages/api/home/home.context';
 
+import DropZone from './Dropzone';
 import ImagePreviewModel from './ImagePreviewModel';
 
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import dayjs from 'dayjs';
 import { v4 } from 'uuid';
 
 const ImageToPromptUpload = () => {
-  const { t } = useTranslation('model');
-  const { t: commonT } = useTranslation('common');
   const { t: imageToPromptT } = useTranslation('imageToPrompt');
 
   const {
@@ -47,9 +27,7 @@ const ImageToPromptUpload = () => {
   const [showImagePreview, setShowImagePreview] = useState<boolean>(false);
   const supabaseClient = useMemo(() => createBrowserSupabaseClient(), []);
 
-  const fileInputOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files?.length) return;
-    const file = e.target.files[0];
+  const fileInputOnChange = (file: File) => {
     if (!file) return;
     const reader = new FileReader();
 
@@ -137,28 +115,8 @@ const ImageToPromptUpload = () => {
       <label className="text-left text-sm text-neutral-700 dark:text-neutral-400 mr-2">
         {imageToPromptT('Image to Prompt')}
       </label>
-      <div
-        className="flex justify-between items-center p-[.4rem]  cursor-pointer w-[50%] rounded-lg border border-neutral-200 bg-transparent text-neutral-900 dark:border-neutral-600 dark:text-white  pr-1 focus:outline-none"
-        onClick={() => {
-          if (!user) {
-            toast.error(
-              commonT('Please sign in to use image to prompt feature'),
-            );
-            return;
-          }
-          document.getElementById('upload-images-to-text')?.click();
-        }}
-      >
-        <div className="text-gray-400">{commonT('Upload')}</div>
-        <IconCirclePlus />
-      </div>
-      <input
-        id="upload-images-to-text"
-        className="sr-only hidden"
-        type="file"
-        accept=".jpg,.jpeg,.png,.gif"
-        onChange={fileInputOnChange}
-      />
+      <DropZone onDropCallback={fileInputOnChange} />
+
       {showImagePreview && (
         <ImagePreviewModel
           onClose={() => {
