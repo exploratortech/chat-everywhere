7f95107fb695cbc9212c0fd074cd625c499c623f
Jack Wu <jackwu833016@gmail.com>
2023-11-12 20:53:41 -0500

    Testing endpoint failure

--- pages/api/v2/image-generation.ts
+++ pages/api/v2/image-generation.ts
@@ -25,7 +25,6 @@ export default async function handler(
 
   console.log('Image generation endpoint is hit');
   res.status(205).end('Missing threadId or messageId or runId');
-  return;
   
   if (req.method !== 'POST') {
     res.setHeader('Allow', 'POST');
