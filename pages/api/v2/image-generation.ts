4f333af7221b8c477c471f775a27469a9a310b8f
Jack Wu <jackwu833016@gmail.com>
2023-11-12 21:21:36 -0500

    Testing endpoint failure

--- pages/api/v2/image-generation.ts
+++ pages/api/v2/image-generation.ts
@@ -45,18 +45,17 @@ export default async function handler(
   const supabase = getAdminSupabaseClient();
   let toolCallId = null;
 
+  // try {
+  const { data: thread, error } = await supabase
+    .from('user_v2_conversations')
+    .select('*')
+    .eq('threadId', threadId)
+    .single();
+
   res.status(200).json({});
   return;
-  
-  // try {
-  //     const { data: thread, error } = await supabase
-  //       .from('user_v2_conversations')
-  //       .select('*')
-  //       .eq('threadId', threadId)
-  //       .single();
-    
-  //     if (error) throw new Error(JSON.stringify(error));
 
+  //     if (error) throw new Error(JSON.stringify(error));
 
   //   if (!thread) {
   //     res.status(404).json({ error: 'Thread not found' });
