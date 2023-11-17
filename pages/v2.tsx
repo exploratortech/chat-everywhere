f4d1897ce10eebf9786c19448fb45037353c3829
Jack Wu <jackwu833016@gmail.com>
2023-11-17 16:25:09 -0500

    Fix loading spinner not being displayed on initial screen

--- pages/v2.tsx
+++ pages/v2.tsx
@@ -158,10 +158,10 @@ const V2Chat = () => {
     setChatMessagesLoading(false);
 
     // Check if requires polling on conversation status
-    if(data.requiresPolling){
+    if (data.requiresPolling) {
       setChatResponseLoading(true);
       setEnablePullingForUpdates(true);
-    }else{
+    } else {
       setChatResponseLoading(false);
       setEnablePullingForUpdates(false);
     }
@@ -170,8 +170,12 @@ const V2Chat = () => {
   const fetchSuggestions = async () => {
     if (!user || !session || enablePullingForUpdates || chatResponseLoading)
       return;
-    
-    if(messages.length === 0 || messages[messages.length - 1].role !== 'assistant') return;
+
+    if (
+      messages.length === 0 ||
+      messages[messages.length - 1].role !== 'assistant'
+    )
+      return;
 
     const response = await fetch('/api/v2/suggestions', {
       method: 'POST',
@@ -304,6 +308,11 @@ const V2Chat = () => {
         />
         <main className="group w-full max-h-screen pl-0 animate-in duration-300 ease-in-out overflow-y-auto pt-5">
           <div className="pb-[120px] mt-12 mb-14">
+            {
+              chatMessagesLoading && (
+                <ConversationLoadingSpinner/>
+              )
+            }
             {messages.length > 0 ? (
               <>
                 <ChatList
@@ -320,7 +329,7 @@ const V2Chat = () => {
                 />
               </>
             ) : (
-              <EmptyScreen />
+              !chatMessagesLoading && <EmptyScreen />
             )}
           </div>
           <ChatPanel
@@ -339,5 +348,28 @@ const V2Chat = () => {
     </TooltipProvider>
   );
 };
+
+const ConversationLoadingSpinner = () => (
+  <div className="relative mx-auto max-w-2xl px-4 h-full">
+    <div className="flex justify-center items-center h-full mt-5">
+      <svg
+        aria-hidden="true"
+        className="w-10 h-10 me-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 mr-2"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+    </div>
+  </div>
+);
 
 export default appWithTranslation(V2Chat);
