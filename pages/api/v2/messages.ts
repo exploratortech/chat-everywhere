bccdb99c79fd1774bf5060621c4e863113981ada
Jack Wu <jackwu833016@gmail.com>
2023-11-17 12:49:40 -0500

    Update /v2/image-generation triggers

--- pages/api/v2/messages.ts
+++ pages/api/v2/messages.ts
@@ -248,7 +248,7 @@ const sendMessage = async (
     });
 
     // Trigger image generation asynchronously
-    fetch(`${process.env.SERVER_HOST}/api/v2/image-generation`, {
+    fetch(`${process.env.SERVER_HOST || `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`}/api/v2/image-generation`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
