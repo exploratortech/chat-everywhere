6d8bf5f0203b4a46a82dc01858384a4c9b7adeb5
Jack Wu <jackwu833016@gmail.com>
2023-11-18 17:03:50 -0500

    Add badge of beta to initial screen

--- components/v2Chat/empty-screen.tsx
+++ components/v2Chat/empty-screen.tsx
@@ -1,10 +1,15 @@
+import { Badge } from './ui/badge';
+
 export function EmptyScreen() {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
-        <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Chat Everywhere v2!
-        </h1>
+        <div className="flex align-middle mb-1">
+          <h1 className="text-lg font-semibold">
+            Welcome to Chat Everywhere v2!
+          </h1>
+          <Badge variant={"outline"} className="ml-2">Beta</Badge>
+        </div>
         <p className="mb-2 leading-normal text-muted-foreground">
           This is the new chat interface for Chat Everywhere. Its currently
           still in beta, please expect a lot of changes.
