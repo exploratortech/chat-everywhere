b1ba98697b9fc67b671d307b930cba6a8d745c08
Jack Wu <jackwu833016@gmail.com>
2023-11-21 21:25:27 -0500

    Use current URL instead of Env variable

--- components/v2Chat/initial-screen.tsx
+++ components/v2Chat/initial-screen.tsx
@@ -10,13 +10,8 @@ export const InitialScreen = () => {
   const supabase = useSupabaseClient();
 
   const getURL = () => {
-    let url =
-      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-      'http://localhost:3000/';
-    // Make sure to include `https://` when not localhost.
-    url = url.includes('http') ? url : `https://${url}/v2`;
-    // Make sure to include a trailing `/`.
+    let url = window.location.hostname;
+    url = `https://${url}/v2`;
     url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
     return url;
   };
