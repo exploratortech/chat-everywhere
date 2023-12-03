e8ff0c24ce6853d549d3ee90b3e634b0db509ac1
Jack Wu <jackwu833016@gmail.com>
2023-12-02 21:40:45 -0500

    Add LineConnectionButton component to Settings_Account

---
+++ components/User/Settings/LineConnectionButton.tsx
@@ -0,0 +1,90 @@
+import { useSession } from '@supabase/auth-helpers-react';
+import React, { useContext, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useTranslation } from 'react-i18next';
+
+import Image from 'next/image';
+
+import HomeContext from '@/pages/api/home/home.context';
+
+export const LineConnectionButton = () => {
+  const {
+    state: { user, isPaidUser },
+    dispatch,
+  } = useContext(HomeContext);
+  const { t } = useTranslation('model');
+  const [disconnecting, setDisconnecting] = useState(false);
+  const session = useSession();
+
+  const lineConnectOnClick = () => {
+    const clientId = process.env.NEXT_PUBLIC_LINE_NOTIFY_CLIENT_ID;
+    const redirectUrl =
+      process.env.NEXT_PUBLIC_ENV === 'production'
+        ? 'https://chateverywhere.app/api/webhooks/line-notify-connect'
+        : `${window.location.protocol}//${window.location.host}/api/webhooks/line-notify-connect`;
+    const lineConnectLink = `https://notify-bot.line.me/oauth/authorize?response_type=code&client_id=${clientId}&scope=notify&state=${session?.access_token}&redirect_uri=${redirectUrl}`;
+    window.location.href = lineConnectLink;
+  };
+
+  const lineDisconnectOnClick = async () => {
+    setDisconnecting(true);
+
+    const response = await fetch('/api/disconnect-with-line', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ accessToken: session?.access_token }),
+    });
+
+    if (!response.ok) {
+      toast.error('Unable to disconnect with LINE, please try again later');
+      setDisconnecting(false);
+      return;
+    }
+
+    dispatch({
+      field: 'user',
+      value: {
+        isConnectedWithLine: false,
+      },
+    });
+    toast.success('Successfully disconnected with LINE');
+    setDisconnecting(false);
+  };
+  return (
+    <div className="flex items-center">
+      <Image
+        src="/assets/line-icon.webp"
+        alt="Line icon"
+        className="inline-block"
+        width="50"
+        height="50"
+      />
+      {user?.isConnectedWithLine ? (
+        <div className="flex items-center">
+          <p className="text-xs text-neutral-400 cursor-default">
+            {t('Connected with LINE')}
+          </p>
+          <button
+            className="border border-neutral-600 hover:bg-gray-200 text-gray-800 p-1 rounded-sm text-xs dark:text-gray-100 dark:hover:bg-transparent ml-2"
+            onClick={lineDisconnectOnClick}
+            disabled={disconnecting}
+          >
+            {disconnecting ? '...' : t('Disconnect')}
+          </button>
+        </div>
+      ) : (
+        <button
+          className={`border border-neutral-600 hover:bg-gray-200 text-gray-800 py-2 px-4 rounded-md text-sm dark:text-gray-100 dark:hover:bg-transparent ${
+            !user || (!isPaidUser && '!text-gray-400')
+          }`}
+          onClick={lineConnectOnClick}
+          disabled={!user || !isPaidUser}
+        >
+          {t('Connect with LINE')}
+        </button>
+      )}
+    </div>
+  );
+};
--- components/User/Settings/Settings_Account.tsx
+++ components/User/Settings/Settings_Account.tsx
@@ -4,7 +4,6 @@ import React, { useContext, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 
-import Image from 'next/image';
 import { event } from 'nextjs-google-analytics';
 
 import { trackEvent } from '@/utils/app/eventTracking';
@@ -13,6 +12,7 @@ import { FeatureItem, PlanDetail } from '@/utils/app/ui';
 import HomeContext from '@/pages/api/home/home.context';
 
 import { ReferralCodeEnter } from '../ReferralCodeEnter';
+import { LineConnectionButton } from './LineConnectionButton';
 
 import dayjs from 'dayjs';
 import LocalizedFormat from 'dayjs/plugin/localizedFormat';
@@ -23,7 +23,6 @@ export default function Settings_Account() {
   const { t } = useTranslation('model');
   const [displayReferralCodeEnterer, setDisplayReferralCodeEnterer] =
     useState(false);
-  const [disconnecting, setDisconnecting] = useState(false);
   const {
     state: { user, isPaidUser },
     dispatch,
@@ -65,43 +64,6 @@ export default function Settings_Account() {
     process.env.NEXT_PUBLIC_ENV === 'production'
       ? 'https://billing.stripe.com/p/login/5kAbMj0wt5VF6AwaEE'
       : 'https://billing.stripe.com/p/login/test_28o4jFe6GaqK1UY5kk';
-
-  const lineConnectOnClick = () => {
-    const clientId = process.env.NEXT_PUBLIC_LINE_NOTIFY_CLIENT_ID;
-    const redirectUrl =
-      process.env.NEXT_PUBLIC_ENV === 'production'
-        ? 'https://chateverywhere.app/api/webhooks/line-notify-connect'
-        : `${window.location.protocol}//${window.location.host}/api/webhooks/line-notify-connect`;
-    const lineConnectLink = `https://notify-bot.line.me/oauth/authorize?response_type=code&client_id=${clientId}&scope=notify&state=${session?.access_token}&redirect_uri=${redirectUrl}`;
-    window.location.href = lineConnectLink;
-  };
-
-  const lineDisconnectOnClick = async () => {
-    setDisconnecting(true);
-
-    const response = await fetch('/api/disconnect-with-line', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ accessToken: session?.access_token }),
-    });
-
-    if (!response.ok) {
-      toast.error('Unable to disconnect with LINE, please try again later');
-      setDisconnecting(false);
-      return;
-    }
-
-    dispatch({
-      field: 'user',
-      value: {
-        isConnectedWithLine: false,
-      },
-    });
-    toast.success('Successfully disconnected with LINE');
-    setDisconnecting(false);
-  };
 
   return (
     <div>
@@ -235,39 +197,7 @@ export default function Settings_Account() {
             {t('Integrations (pro plan)')}
           </span>
         </div>
-        <div className="flex items-center">
-          <Image
-            src="/assets/line-icon.webp"
-            alt="Line icon"
-            className="inline-block"
-            width="50"
-            height="50"
-          />
-          {user?.isConnectedWithLine ? (
-            <div className="flex items-center">
-              <p className="text-xs text-neutral-400 cursor-default">
-                {t('Connected with LINE')}
-              </p>
-              <button
-                className="border border-neutral-600 hover:bg-gray-200 text-gray-800 p-1 rounded-sm text-xs dark:text-gray-100 dark:hover:bg-transparent ml-2"
-                onClick={lineDisconnectOnClick}
-                disabled={disconnecting}
-              >
-                {disconnecting ? '...' : t('Disconnect')}
-              </button>
-            </div>
-          ) : (
-            <button
-              className={`border border-neutral-600 hover:bg-gray-200 text-gray-800 py-2 px-4 rounded-md text-sm dark:text-gray-100 dark:hover:bg-transparent ${
-                !user || (!isPaidUser && '!text-gray-400')
-              }`}
-              onClick={lineConnectOnClick}
-              disabled={!user || !isPaidUser}
-            >
-              {t('Connect with LINE')}
-            </button>
-          )}
-        </div>
+        <LineConnectionButton />
       </div>
     </div>
   );
