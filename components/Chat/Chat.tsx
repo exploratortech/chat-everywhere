372f81f2ace700adff3c0addbb410bc9d6bca66c
1orzero <laokameng.6@gmail.com>
2024-01-02 16:41:21 +0800

    Refactor handleSend

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -31,7 +31,7 @@ import { getOrGenerateUserId } from '@/utils/data/taggingHelper';
 import { throttle } from '@/utils/data/throttle';
 
 import { ChatBody, Conversation, Message } from '@/types/chat';
-import { PluginID, Plugins } from '@/types/plugin';
+import { Plugin, PluginID, Plugins } from '@/types/plugin';
 
 import HomeContext from '@/pages/api/home/home.context';
 
@@ -90,235 +90,38 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       const plugin = (message.pluginId && Plugins[message.pluginId]) || null;
 
       if (selectedConversation) {
-        let updatedConversation: Conversation;
-        if (deleteCount) {
-          const updatedMessages = [...selectedConversation.messages];
-
-          for (let i = 0; i < deleteCount; i++) {
-            updatedMessages.pop();
-          }
-
-          updatedConversation = {
-            ...selectedConversation,
-            messages: [...updatedMessages, message],
-          };
-        } else {
-          updatedConversation = {
-            ...selectedConversation,
-            messages: [...selectedConversation.messages, message],
-          };
-        }
-        homeDispatch({
-          field: 'selectedConversation',
-          value: updatedConversation,
-        });
-        homeDispatch({ field: 'loading', value: true });
-        homeDispatch({ field: 'messageIsStreaming', value: true });
-        const chatBody: ChatBody = {
-          model: updatedConversation.model,
-          messages: updatedConversation.messages,
-          prompt: updatedConversation.prompt,
-          temperature: updatedConversation.temperature,
-        };
-        const endpoint = getEndpoint(plugin);
-
-        if (plugin?.id === PluginID.IMAGE_GEN) {
-          chatBody.imageQuality =
-            selectedConversation.imageQuality ||
-            DEFAULT_IMAGE_GENERATION_QUALITY;
-          chatBody.imageStyle =
-            selectedConversation.imageStyle || DEFAULT_IMAGE_GENERATION_STYLE;
-        }
-
-        const body = JSON.stringify(chatBody);
         const controller = new AbortController();
-        const response = await fetch(endpoint, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Output-Language': outputLanguage,
-            'user-token': user?.token || '',
-            'user-browser-id': getOrGenerateUserId() || '',
-            'user-selected-plugin-id': plugin?.id || '',
-          },
-          signal: controller.signal,
-          body,
-        });
-        if (!response.ok) {
-          homeDispatch({ field: 'loading', value: false });
-          homeDispatch({ field: 'messageIsStreaming', value: false });
-          if (response.status === 429) {
-            toast.error(
-              t(
-                'We apologize for the inconvenience, but our server is currently experiencing high traffic. Please try again later.',
-              ),
-            );
-          } else if (response.status === 401) {
-            toast.error(
-              t(
-                'Sorry something went wrong. Please refresh the page and try again.',
-              ),
-            );
-          } else {
-            toast.error(
-              t(
-                'Sorry, something went wrong. Our team has been notified and will look into it.',
-              ),
-            );
-          }
+        const updatedConversation = updateConversation(
+          deleteCount,
+          message,
+          selectedConversation,
+        );
+        const chatBody = createChatBody(
+          updatedConversation,
+          plugin,
+          selectedConversation,
+        );
+        const response = await sendRequest(chatBody, plugin, controller);
 
-          // remove the last message from the conversation
-          homeDispatch({
-            field: 'selectedConversation',
-            value: {
-              ...selectedConversation,
-              messages: [...selectedConversation.messages],
-            },
-          });
+        if (!response.ok) {
+          handleErrorResponse(response, selectedConversation);
           return;
         }
+
         const data = response.body;
         if (!data) {
-          homeDispatch({ field: 'loading', value: false });
-          homeDispatch({ field: 'messageIsStreaming', value: false });
+          handleNoDataResponse();
           return;
         }
 
-        if (updatedConversation.messages.length === 1) {
-          const { content } = message;
-          const customName =
-            content.length > 30 ? content.substring(0, 30) + '...' : content;
-          updatedConversation = {
-            ...updatedConversation,
-            name: customName,
-          };
-        }
-        homeDispatch({ field: 'loading', value: false });
-        const reader = data.getReader();
-        const decoder = new TextDecoder();
-        let done = false;
-        let isFirst = true;
-        let text = '';
-        let largeContextResponse = false;
-        let showHintForLargeContextResponse = false;
-
-        while (!done) {
-          if (stopConversationRef.current === true) {
-            controller.abort();
-            done = true;
-            break;
-          }
-          const { value, done: doneReading } = await reader.read();
-          done = doneReading;
-          const chunkValue = decoder.decode(value);
-          text += chunkValue;
-
-          if (text.includes('[DONE]')) {
-            text = text.replace('[DONE]', '');
-            done = true;
-          }
-
-          if (text.includes('[16K]')) {
-            text = text.replace('[16K]', '');
-            largeContextResponse = true;
-          }
-
-          if (text.includes('[16K-Optional]')) {
-            text = text.replace('[16K-Optional]', '');
-            showHintForLargeContextResponse = true;
-          }
-
-          if (text.includes('[REMOVE_TEMP_HTML]')) {
-            text = removeTempHtmlString(text);
-          }
-
-          if (text.includes('[REMOVE_LAST_LINE]')) {
-            text = text.replace('[REMOVE_LAST_LINE]', '');
-            text = removeSecondLastLine(text);
-          }
-
-          // We can use this command to trigger the initial stream of Edge function response
-          // so we have more than 25 seconds on Vercel Edge network to wait for response
-          if (text.includes('[PLACEHOLDER]')) {
-            text = text.replace('[PLACEHOLDER]', '');
-          }
-
-          if (isFirst) {
-            isFirst = false;
-            const updatedMessages: Message[] = [
-              ...updatedConversation.messages,
-              {
-                role: 'assistant',
-                content: removeRedundantTempHtmlString(text),
-                largeContextResponse,
-                showHintForLargeContextResponse,
-                pluginId: plugin?.id || null,
-              },
-            ];
-            updatedConversation = {
-              ...updatedConversation,
-              messages: updatedMessages,
-              lastUpdateAtUTC: dayjs().valueOf(),
-            };
-            homeDispatch({
-              field: 'selectedConversation',
-              value: updatedConversation,
-            });
-          } else {
-            const updatedMessages: Message[] = updatedConversation.messages.map(
-              (message, index) => {
-                if (index === updatedConversation.messages.length - 1) {
-                  return {
-                    ...message,
-                    content: removeRedundantTempHtmlString(text),
-                    largeContextResponse,
-                    showHintForLargeContextResponse,
-                  };
-                }
-                return message;
-              },
-            );
-            updatedConversation = {
-              ...updatedConversation,
-              messages: updatedMessages,
-              lastUpdateAtUTC: dayjs().valueOf(),
-            };
-            homeDispatch({
-              field: 'selectedConversation',
-              value: updatedConversation,
-            });
-          }
-        }
-
-        saveConversation(updatedConversation);
-        let updatedConversations: Conversation[] = conversations.map(
-          (conversation) => {
-            if (conversation.id === selectedConversation.id) {
-              return updatedConversation;
-            }
-            return conversation;
-          },
+        handleDataResponse(
+          data,
+          updatedConversation,
+          plugin,
+          message,
+          controller,
+          selectedConversation,
         );
-
-        // If the conversation is new, add it to the list of conversations
-        if (
-          !updatedConversations.find(
-            (conversation) => conversation.id === updatedConversation.id,
-          )
-        ) {
-          updatedConversations.push(updatedConversation);
-          updatedConversations = reorderItem(
-            updatedConversations,
-            updatedConversation.id,
-            0,
-          );
-        }
-
-        homeDispatch({ field: 'conversations', value: updatedConversations });
-        saveConversations(updatedConversations);
-        homeDispatch({ field: 'messageIsStreaming', value: false });
-
-        updateConversationLastUpdatedAtTimeStamp();
       }
     },
     [
@@ -330,6 +133,268 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       homeDispatch,
     ],
   );
+
+  function updateConversation(
+    deleteCount: number,
+    message: Message,
+    selectedConversation: Conversation,
+  ): Conversation {
+    let updatedConversation: Conversation;
+    if (deleteCount) {
+      const updatedMessages = [...selectedConversation.messages];
+
+      for (let i = 0; i < deleteCount; i++) {
+        updatedMessages.pop();
+      }
+
+      updatedConversation = {
+        ...selectedConversation,
+        messages: [...updatedMessages, message],
+      };
+    } else {
+      updatedConversation = {
+        ...selectedConversation,
+        messages: [...selectedConversation.messages, message],
+      };
+    }
+    homeDispatch({
+      field: 'selectedConversation',
+      value: updatedConversation,
+    });
+    homeDispatch({ field: 'loading', value: true });
+    homeDispatch({ field: 'messageIsStreaming', value: true });
+
+    return updatedConversation;
+  }
+
+  function createChatBody(
+    updatedConversation: Conversation,
+    plugin: Plugin | null,
+    selectedConversation: Conversation,
+  ): ChatBody {
+    const chatBody: ChatBody = {
+      model: updatedConversation.model,
+      messages: updatedConversation.messages,
+      prompt: updatedConversation.prompt,
+      temperature: updatedConversation.temperature,
+    };
+
+    if (plugin?.id === PluginID.IMAGE_GEN) {
+      chatBody.imageQuality =
+        selectedConversation.imageQuality || DEFAULT_IMAGE_GENERATION_QUALITY;
+      chatBody.imageStyle =
+        selectedConversation.imageStyle || DEFAULT_IMAGE_GENERATION_STYLE;
+    }
+
+    return chatBody;
+  }
+
+  async function sendRequest(
+    chatBody: ChatBody,
+    plugin: Plugin | null,
+    controller: AbortController,
+  ): Promise<Response> {
+    const body = JSON.stringify(chatBody);
+
+    const response = await fetch(getEndpoint(plugin), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Output-Language': outputLanguage,
+        'user-token': user?.token || '',
+        'user-browser-id': getOrGenerateUserId() || '',
+        'user-selected-plugin-id': plugin?.id || '',
+      },
+      signal: controller.signal,
+      body,
+    });
+
+    return response;
+  }
+
+  function handleErrorResponse(
+    response: Response,
+    selectedConversation: Conversation,
+  ) {
+    homeDispatch({ field: 'loading', value: false });
+    homeDispatch({ field: 'messageIsStreaming', value: false });
+    if (response.status === 429) {
+      toast.error(
+        t(
+          'We apologize for the inconvenience, but our server is currently experiencing high traffic. Please try again later.',
+        ),
+      );
+    } else if (response.status === 401) {
+      toast.error(
+        t('Sorry something went wrong. Please refresh the page and try again.'),
+      );
+    } else {
+      toast.error(
+        t(
+          'Sorry, something went wrong. Our team has been notified and will look into it.',
+        ),
+      );
+    }
+
+    // remove the last message from the conversation
+    homeDispatch({
+      field: 'selectedConversation',
+      value: {
+        ...selectedConversation,
+        messages: [...selectedConversation.messages],
+      },
+    });
+  }
+
+  function handleNoDataResponse() {
+    homeDispatch({ field: 'loading', value: false });
+    homeDispatch({ field: 'messageIsStreaming', value: false });
+  }
+
+  async function handleDataResponse(
+    data: ReadableStream<Uint8Array>,
+    updatedConversation: Conversation,
+    plugin: Plugin | null,
+    message: Message,
+    controller: AbortController,
+    selectedConversation: Conversation,
+  ) {
+    if (updatedConversation.messages.length === 1) {
+      const { content } = message;
+      const customName =
+        content.length > 30 ? content.substring(0, 30) + '...' : content;
+      updatedConversation = {
+        ...updatedConversation,
+        name: customName,
+      };
+    }
+    homeDispatch({ field: 'loading', value: false });
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+    let isFirst = true;
+    let text = '';
+    let largeContextResponse = false;
+    let showHintForLargeContextResponse = false;
+
+    while (!done) {
+      if (stopConversationRef.current === true) {
+        controller.abort();
+        done = true;
+        break;
+      }
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      text += chunkValue;
+
+      if (text.includes('[DONE]')) {
+        text = text.replace('[DONE]', '');
+        done = true;
+      }
+
+      if (text.includes('[16K]')) {
+        text = text.replace('[16K]', '');
+        largeContextResponse = true;
+      }
+
+      if (text.includes('[16K-Optional]')) {
+        text = text.replace('[16K-Optional]', '');
+        showHintForLargeContextResponse = true;
+      }
+
+      if (text.includes('[REMOVE_TEMP_HTML]')) {
+        text = removeTempHtmlString(text);
+      }
+
+      if (text.includes('[REMOVE_LAST_LINE]')) {
+        text = text.replace('[REMOVE_LAST_LINE]', '');
+        text = removeSecondLastLine(text);
+      }
+
+      // We can use this command to trigger the initial stream of Edge function response
+      // so we have more than 25 seconds on Vercel Edge network to wait for response
+      if (text.includes('[PLACEHOLDER]')) {
+        text = text.replace('[PLACEHOLDER]', '');
+      }
+
+      if (isFirst) {
+        isFirst = false;
+        const updatedMessages: Message[] = [
+          ...updatedConversation.messages,
+          {
+            role: 'assistant',
+            content: removeRedundantTempHtmlString(text),
+            largeContextResponse,
+            showHintForLargeContextResponse,
+            pluginId: plugin?.id || null,
+          },
+        ];
+        updatedConversation = {
+          ...updatedConversation,
+          messages: updatedMessages,
+          lastUpdateAtUTC: dayjs().valueOf(),
+        };
+        homeDispatch({
+          field: 'selectedConversation',
+          value: updatedConversation,
+        });
+      } else {
+        const updatedMessages: Message[] = updatedConversation.messages.map(
+          (message, index) => {
+            if (index === updatedConversation.messages.length - 1) {
+              return {
+                ...message,
+                content: removeRedundantTempHtmlString(text),
+                largeContextResponse,
+                showHintForLargeContextResponse,
+              };
+            }
+            return message;
+          },
+        );
+        updatedConversation = {
+          ...updatedConversation,
+          messages: updatedMessages,
+          lastUpdateAtUTC: dayjs().valueOf(),
+        };
+        homeDispatch({
+          field: 'selectedConversation',
+          value: updatedConversation,
+        });
+      }
+    }
+
+    saveConversation(updatedConversation);
+    let updatedConversations: Conversation[] = conversations.map(
+      (conversation) => {
+        if (conversation.id === selectedConversation.id) {
+          return updatedConversation;
+        }
+        return conversation;
+      },
+    );
+
+    // If the conversation is new, add it to the list of conversations
+    if (
+      !updatedConversations.find(
+        (conversation) => conversation.id === updatedConversation.id,
+      )
+    ) {
+      updatedConversations.push(updatedConversation);
+      updatedConversations = reorderItem(
+        updatedConversations,
+        updatedConversation.id,
+        0,
+      );
+    }
+
+    homeDispatch({ field: 'conversations', value: updatedConversations });
+    saveConversations(updatedConversations);
+    homeDispatch({ field: 'messageIsStreaming', value: false });
+
+    updateConversationLastUpdatedAtTimeStamp();
+  }
 
   const handleScroll = throttle(() => {
     if (chatContainerRef.current) {
