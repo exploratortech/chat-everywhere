29d520b164dd5a0a716cca7327ff9977c692686f
Jack Wu <jackwu833016@gmail.com>
2024-01-13 15:40:44 -0500

    Only show AI speech config to paying users

--- components/User/Settings/Settings_App.tsx
+++ components/User/Settings/Settings_App.tsx
@@ -4,14 +4,15 @@ import { useTranslation } from 'react-i18next';
 
 import HomeContext from '@/pages/api/home/home.context';
 
-import { useAzureTts, SpeechSpeedType } from '@/components/Hooks/useAzureTts';
+import { SpeechSpeedType, useAzureTts } from '@/components/Hooks/useAzureTts';
 import { SidebarButton } from '@/components/Sidebar/SidebarButton';
 
 export default function Settings_App() {
   const { t } = useTranslation('model');
-  const [speechSpeedConfig, setSpeechSpeedConfig] = useState<SpeechSpeedType>("normal");
+  const [speechSpeedConfig, setSpeechSpeedConfig] =
+    useState<SpeechSpeedType>('normal');
   const {
-    state: { lightMode },
+    state: { lightMode, isPaidUser },
     dispatch: homeDispatch,
   } = useContext(HomeContext);
 
@@ -41,32 +42,39 @@ export default function Settings_App() {
           })
         }
       />
-      <div className="inline-flex items-center justify-center w-full">
-        <hr className="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-        <span className="absolute px-3 text-white -translate-x-1/2 left-1/2 bg-[#171717]">
-          {t('Speech configuration')}
-        </span>
-      </div>
-      <div className="flex mt-4 md:space-x-4 bg-transparent items-center">
-        <div className="md:w-1/2">
-          <label htmlFor="speechSpeed" className="block text-sm font-medium">
-            {t('Speech Speed')}
-          </label>
-        </div>
-        <div className="md:w-1/2">
-          <select
-            id="speechSpeed"
-            name="speechSpeed"
-            value={speechSpeedConfig}
-            onChange={(e) => speechSpeedOnChange(e.target.value)}
-            className="mt-1 block w-full pl-3 pr-10 py-2 text-base focus:outline-none sm:text-sm rounded-md bg-transparent text-end"
-          >
-            <option value={'slow'}>{t('Slow')}</option>
-            <option value={'normal'}>{t('Normal')}</option>
-            <option value={'fast'}>{t('Fast')}</option>
-          </select>
-        </div>
-      </div>
+      {isPaidUser && (
+        <>
+          <div className="inline-flex items-center justify-center w-full">
+            <hr className="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+            <span className="absolute px-3 text-white -translate-x-1/2 left-1/2 bg-[#171717]">
+              {t('Speech configuration')}
+            </span>
+          </div>
+          <div className="flex mt-4 md:space-x-4 bg-transparent items-center">
+            <div className="md:w-1/2">
+              <label
+                htmlFor="speechSpeed"
+                className="block text-sm font-medium"
+              >
+                {t('Speech Speed')}
+              </label>
+            </div>
+            <div className="md:w-1/2">
+              <select
+                id="speechSpeed"
+                name="speechSpeed"
+                value={speechSpeedConfig}
+                onChange={(e) => speechSpeedOnChange(e.target.value)}
+                className="mt-1 block w-full pl-3 pr-10 py-2 text-base focus:outline-none sm:text-sm rounded-md bg-transparent text-end"
+              >
+                <option value={'slow'}>{t('Slow')}</option>
+                <option value={'normal'}>{t('Normal')}</option>
+                <option value={'fast'}>{t('Fast')}</option>
+              </select>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
