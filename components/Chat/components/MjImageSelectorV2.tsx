4da734a0e3e5e2a5c8ea2b221dbdc9ce366a69f9
1orzero <laokameng.6@gmail.com>
2024-01-22 23:29:22 +0800

    Add MjImageSelectorV2 & MjImageComponentV2 component and update streamHandler and mjImage

---
+++ components/Chat/components/MjImageSelectorV2.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+
+export interface MjImageItem {
+  imageUrl: string;
+  imageAlt: string;
+  buttons: string[];
+}
+export interface MjImageSelectorV2Props {
+  previousButtonCommand: string;
+  imageUrl: string;
+  buttons: string[];
+  buttonMessageId: string;
+  prompt: string;
+}
+
+// THE COMPONENT IS USED FOR STATIC HTML GENERATION, SO DON'T USE HOOKS OR STATE
+export default function MjImageSelectorV2({
+  previousButtonCommand,
+  imageUrl,
+  buttons,
+  buttonMessageId,
+  prompt,
+}: MjImageSelectorV2Props) {
+  const upscalePattern = /^U\d$/i;
+  const isPreviousUpscaleCommand = upscalePattern.test(previousButtonCommand);
+
+  return (
+    <div id="mj-image-static-v2" className="my-4">
+      {/* // DO NOT EDIT THE <IMG> TAG BELOW. IT WILL BE SWAPPED OUT BY THE ReactMarkdown COMPONENT. */}
+      <img
+        key={`mj-image-${imageUrl}`}
+        src={imageUrl}
+        alt={imageUrl}
+        data-ai-image-buttons={buttons.join(',')}
+        data-ai-image-button-message-id={buttonMessageId}
+        data-ai-image-prompt={prompt}
+      />
+    </div>
+  );
+}
--- utils/app/mjImage.ts
+++ utils/app/mjImage.ts
@@ -2,6 +2,64 @@ import { Conversation } from '@/types/chat';
 
 import { load } from 'cheerio';
 
+export const getUpdatedAssistantMjConversationV2 = (
+  selectedConversation: Conversation,
+  buttonMessageId: string,
+) => {
+  // find the message with role assistant, pluginId image-gen, and content includes the buttonMessageId
+  const assistantImgGenMessageContent = selectedConversation?.messages.filter(
+    (message) => {
+      return (
+        message.role === 'assistant' &&
+        message.pluginId === 'image-gen' &&
+        message.content.includes(buttonMessageId)
+      );
+    },
+  );
+
+  if (!assistantImgGenMessageContent) return;
+
+  console.log(assistantImgGenMessageContent);
+  const oldContent = assistantImgGenMessageContent[0].content;
+
+  // find the html tag with id mj-image-static-v2, return the content
+  const mjImageSelection = oldContent.match(
+    /(<div id="mj-image-static-v2" (.*)>(.*)<\/div>)/,
+  );
+
+  const html = mjImageSelection?.[0];
+  if (!html) return;
+
+  const $ = load(html);
+  // find all img tag , add data-ai-image-button-commands-executed="1" to all the image tag
+  const images = $('img');
+  images.each((index, element) => {
+    $(element).attr('data-ai-image-button-commands-executed', '1');
+  });
+  const result = $('#mj-image-static-v2').prop('outerHTML')!;
+
+  // update the message content with the new html string, replace the old html string
+  const newContent = oldContent.replace(
+    /(<div id="mj-image-static-v2" (.*)>(.*)<\/div>)/,
+    result,
+  );
+  selectedConversation.messages = selectedConversation.messages.map(
+    (message) => {
+      if (
+        message.role === 'assistant' &&
+        message.pluginId === 'image-gen' &&
+        message.content.includes(buttonMessageId)
+      ) {
+        message.content = newContent;
+        return message;
+      } else {
+        return message;
+      }
+    },
+  );
+  return selectedConversation;
+};
+
 export const getUpdatedAssistantMjConversation = (
   selectedConversation: Conversation,
   buttonMessageId: string,
@@ -19,6 +77,7 @@ export const getUpdatedAssistantMjConversation = (
 
   if (!assistantImgGenMessageContent) return;
 
+  console.log(assistantImgGenMessageContent);
   const oldContent = assistantImgGenMessageContent[0].content;
 
   // find the html tag with id mj-image-selection, return the content
