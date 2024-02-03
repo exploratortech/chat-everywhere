55fae165c3849d376b0045190e4a3dbc1781cb65
1orzero <laokameng.6@gmail.com>
2024-02-03 18:40:58 +0800

    Add useState hook and display custom instruction modal

--- components/Chat/CustomInstructionInUseIndicator.tsx
+++ components/Chat/CustomInstructionInUseIndicator.tsx
@@ -1,9 +1,11 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { PluginID } from '@/types/plugin';
 
 import HomeContext from '@/pages/api/home/home.context';
+
+import CustomInstructionInUseDisplayModel from '../Promptbar/components/CustomInstructionInUseDisplayModel';
 
 const CustomInstructionInUseIndicator = () => {
   const { t } = useTranslation('chat');
@@ -14,6 +16,8 @@ const CustomInstructionInUseIndicator = () => {
     currentMessage?.pluginId === null ||
     currentMessage?.pluginId === PluginID.LANGCHAIN_CHAT ||
     currentMessage?.pluginId === PluginID.GPT4;
+
+  const [showModal, setShowModal] = useState(false);
   if (selectedConversation?.customInstructionPrompt && isInChatMode) {
     const customInstructionPromptName =
       selectedConversation.customInstructionPrompt.name.trim().length > 10
@@ -22,24 +26,46 @@ const CustomInstructionInUseIndicator = () => {
             .substring(0, 10) + '...'
         : selectedConversation.customInstructionPrompt.name.trim();
     return (
-      <div className="group relative h-2 w-2 mx-2 cursor-pointer">
-        <div className="h-2 w-2 bg-green-500 rounded-full absolute"></div>
-        <div className="h-2 w-2 bg-green-500 rounded-full animate-ping absolute"></div>
-        <span className="absolute top-6 scale-0 rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100 !w-max">
-          {t('Custom Instruction ({{customInstructionPromptName}}) is in use', {
-            customInstructionPromptName,
-          })}
-        </span>
-      </div>
+      <>
+        <div
+          className="group relative h-2 w-2 mx-2 cursor-pointer"
+          onClick={() => setShowModal(true)}
+        >
+          <div className="h-2 w-2 bg-green-500 rounded-full absolute"></div>
+          <div className="h-2 w-2 bg-green-500 rounded-full animate-ping absolute"></div>
+          <span className="absolute top-6 scale-0 rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100 !w-max">
+            {t(
+              'Custom Instruction ({{customInstructionPromptName}}) is in use',
+              {
+                customInstructionPromptName,
+              },
+            )}
+          </span>
+        </div>
+        {showModal && (
+          <CustomInstructionInUseDisplayModel
+            prompt={selectedConversation.customInstructionPrompt}
+            onClose={() => setShowModal(false)}
+          />
+        )}
+      </>
     );
   } else if (selectedConversation?.customInstructionPrompt) {
     return (
-      <div className="group relative h-2 w-2 mx-2">
-        <div className="h-2 w-2 bg-red-500 rounded-full absolute"></div>
-        <span className="absolute top-6 scale-0 rounded bg-gray-800 p-2 text-xs text-white !w-max">
-          {`Custom Instruction only available in Chat mode`}
-        </span>
-      </div>
+      <>
+        <div className="group relative h-2 w-2 mx-2">
+          <div className="h-2 w-2 bg-red-500 rounded-full absolute"></div>
+          <span className="absolute top-6 scale-0 rounded bg-gray-800 p-2 text-xs text-white !w-max">
+            {`Custom Instruction only available in Chat mode`}
+          </span>
+        </div>
+        {showModal && (
+          <CustomInstructionInUseDisplayModel
+            prompt={selectedConversation.customInstructionPrompt}
+            onClose={() => setShowModal(false)}
+          />
+        )}
+      </>
     );
   }
 
