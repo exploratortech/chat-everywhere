82e6c86a10a277fe7af5cb280c77f5ab5016205f
1orzero <laokameng.6@gmail.com>
2024-02-16 00:23:24 +0800

    Fix i18n hydration issue

--- pages/teacher-portal/index.tsx
+++ pages/teacher-portal/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useRouter } from 'next/router';
 
@@ -22,6 +22,15 @@ const TeacherPortal = () => {
   const {
     state: { showing },
   } = PortalState;
+
+  // State to track if the component has mounted (i.e., we're on the client side)
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    // Set hasMounted to true when the component mounts
+    setHasMounted(true);
+  }, []);
+
   return (
     <DefaultLayout>
       <TeacherPortalContext.Provider value={{ ...PortalState }}>
@@ -29,7 +38,9 @@ const TeacherPortal = () => {
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center text-center mobile:block">
             <div className="w-full tablet:max-w-[90vw] transform overflow-hidden text-left align-middle shadow-xl transition-all bg-neutral-800 text-neutral-200 flex h-[100dvh] tablet:max-h-[unset] !max-w-[unset] !rounded-none">
-              <Sidebar className="bg-neutral-800 flex-shrink-0 flex-grow-0" />
+              {hasMounted && (
+                <Sidebar className="bg-neutral-800 flex-shrink-0 flex-grow-0" />
+              )}
               <div className="p-6 bg-neutral-900 flex-grow relative overflow-y-auto">
                 {showing === 'one-time-code' && <OneTimeCodeGeneration />}
                 {showing === 'shared-message' && <SharedMessages />}
