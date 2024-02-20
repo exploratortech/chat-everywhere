67eaa8f061dd2eeadd4fe63078c6cefb6affa9e5
williamtriinh <williamtrinh2039@gmail.com>
2024-02-20 14:18:06 -0500

    Install tailwindcss forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -106,5 +106,9 @@ module.exports = {
       visibility: ['group-hover'],
     },
   },
-  plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-animate'),
+  ],
 };
