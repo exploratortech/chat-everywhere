a79cfb78f98e014144fb9c0832c7d9a8a050f419
williamtriinh <williamtrinh2039@gmail.com>
2024-02-22 14:57:36 -0500

    Open the folder when a conversation is added to it

--- components/Folder/Folder.tsx
+++ components/Folder/Folder.tsx
@@ -214,6 +214,7 @@ const Folder = ({
                 handleClick={(e) => {
                   e.stopPropagation();
                   handleNewConversation(currentFolder.id);
+                  setIsOpen(true);
                 }}
               >
                 <IconPlus size={18} />
