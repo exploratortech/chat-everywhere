e16d535e9e6211cd1ae5db154acc9a1c67ac69b4
1orzero <laokameng.6@gmail.com>
2024-02-26 12:08:41 +0800

    Refactor Tag component to allow optional count and onSelect props

--- components/TeacherPortal/Tags/Tag.tsx
+++ components/TeacherPortal/Tags/Tag.tsx
@@ -2,22 +2,26 @@ import { cn } from '@/lib/utils';
 
 const Tag = ({
   label,
-  count,
+  count = 0,
   onSelect,
   selected,
 }: {
   label: string;
-  count: number;
-  onSelect: () => void;
-  selected: boolean;
+  count?: number;
+  onSelect?: () => void;
+  selected?: boolean;
 }) => {
   return (
     <div
       className={cn(
-        'cursor-pointer hover:bg-neutral-700 border inline-flex items-center h-max bg-neutral-800 text-white text-sm font-semibold px-2.5 py-0.5 rounded-lg',
+        'hover:bg-neutral-700 border inline-flex items-center h-max bg-neutral-800 text-white text-sm font-semibold px-2.5 py-0.5 rounded-lg',
         selected ? 'bg-neutral-700' : 'border-neutral-600',
+        onSelect ? 'cursor-pointer' : 'cursor-default',
       )}
-      onClick={onSelect}
+      onClick={(e) => {
+        e.stopPropagation();
+        onSelect?.();
+      }}
     >
       <span className="px-1.5 py-0.5 rounded mr-1">#{label}</span>
       {count > 0 && (
