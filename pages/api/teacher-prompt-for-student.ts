dca8211a8094157efe708ce75a129d74f401cdae
1orzero <laokameng.6@gmail.com>
2024-02-27 11:52:30 +0800

    Add teacher-prompt-for-student API endpoint

---
+++ pages/api/teacher-prompt-for-student.ts
@@ -0,0 +1,30 @@
+import {
+  fetchUserProfileWithAccessToken,
+  unauthorizedResponse,
+} from '@/utils/server/auth';
+import { getTeacherPromptForStudent } from '@/utils/server/supabase/teacher-prompt';
+
+export const config = {
+  runtime: 'edge',
+};
+
+const handler = async (req: Request): Promise<Response> => {
+  const userProfile = await fetchUserProfileWithAccessToken(req);
+  if (!userProfile || !userProfile.isTempUser) return unauthorizedResponse;
+
+  try {
+    return new Response(
+      JSON.stringify({
+        prompts: await getTeacherPromptForStudent(userProfile.id),
+      }),
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response('Error', {
+      status: 500,
+      statusText: 'Internal server error',
+    });
+  }
+};
+export default handler;
