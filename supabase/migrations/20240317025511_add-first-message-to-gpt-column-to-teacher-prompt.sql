0524520633e4a4a12225df15c85af488d942cd4c
1orzero <laokameng.6@gmail.com>
2024-03-17 10:55:27 +0800

    Add first_message_to_gpt column to teacher_prompts table

---
+++ supabase/migrations/20240317025511_add-first-message-to-gpt-column-to-teacher-prompt.sql
@@ -0,0 +1,3 @@
+alter table "public"."teacher_prompts" add column "first_message_to_gpt" text default 'Provide a very short welcome message based on your prompt, the role your are playing is based on the prompt.'::text;
+
+
