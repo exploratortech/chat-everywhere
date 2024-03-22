d14de59a76fab1cce83bb0ed63f7552d16c54276
1orzero <laokameng.6@gmail.com>
2024-03-22 13:07:22 +0800

    Add function to retrieve teacher settings for a student

--- utils/server/supabase/teacher-settings.ts
+++ utils/server/supabase/teacher-settings.ts
@@ -7,6 +7,48 @@ import { getAdminSupabaseClient } from '../supabase';
 
 const supabase = getAdminSupabaseClient();
 
+export async function getShouldClearConversationsOnLogoutForStudent(
+  student_profile_id: string,
+): Promise<TeacherSettingsInPortal['should_clear_conversations_on_logout']> {
+  // Step 1: Locate teacher profile id by getting the temp profile by student profile id
+  // with join on one time code table to get the teacher profile id
+  const tempProfileRes = await supabase
+    .from('temporary_account_profiles')
+    .select('one_time_codes(teacher_profile_id)')
+    .eq('profile_id', student_profile_id)
+    .single();
+
+  if (tempProfileRes.error || !tempProfileRes.data) {
+    throw tempProfileRes.error;
+  }
+
+  if (
+    !tempProfileRes.data.one_time_codes ||
+    tempProfileRes.data.one_time_codes.length === 0
+  ) {
+    console.log('No one time code found for the student profile id');
+    throw new Error('No one time code found for the student profile id');
+  }
+  const teacher_profile_id = (tempProfileRes.data.one_time_codes as any)
+    .teacher_profile_id;
+
+  // Step 2: Get the teacher settings by teacher profile id and filter out
+  const { data, error } = await supabase
+    .from('teacher_settings')
+    .select('should_clear_conversations_on_logout')
+    .eq('teacher_profile_id', teacher_profile_id);
+
+  if (error) {
+    console.log(error);
+    throw error;
+  }
+  if (!data?.length) {
+    console.log('No teacher settings found for the teacher profile id');
+    throw new Error('No teacher settings found for the teacher profile id');
+  }
+
+  return data[0].should_clear_conversations_on_logout;
+}
 export async function getTeacherSettingsForStudent(
   student_profile_id: string,
 ): Promise<TeacherSettings> {
