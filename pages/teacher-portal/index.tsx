ff8c6767449565427e53a38ae616a8dbe8ac6d33
1orzero <laokameng.6@gmail.com>
2024-03-29 14:59:18 +0800

    Redirect teacher portal index page to one-time code page

--- pages/teacher-portal/index.tsx
+++ pages/teacher-portal/index.tsx
@@ -3,15 +3,13 @@ import { GetServerSideProps } from 'next';
 export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     redirect: {
-      destination: '/teacher-portal/one-time-code', // Target route
-      permanent: false, // Temporary redirect, set to true if it's a permanent redirect
+      destination: '/teacher-portal/one-time-code',
+      permanent: false,
     },
   };
 };
 
 const TeacherPortalIndex = () => {
-  // This component will not actually render because of the server-side redirect,
-  // but you can return a simple component or loading state if needed.
   return null;
 };
 
