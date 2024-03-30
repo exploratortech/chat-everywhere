7ac289a195f5d53a9888e2dce7f9a115ad50203a
1orzero <laokameng.6@gmail.com>
2024-03-30 22:53:40 +0800

    Redirect teacher portal index page to one-time code page

--- pages/teacher-portal/index.tsx
+++ pages/teacher-portal/index.tsx
@@ -1,16 +1,19 @@
-import { GetServerSideProps } from 'next';
+import { useEffect } from 'react';
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  return {
-    redirect: {
-      destination: '/teacher-portal/one-time-code',
-      permanent: false,
-    },
-  };
-};
+import { useRouter } from 'next/router';
+
+import { withCommonServerSideProps } from '@/utils/withCommonServerSideProps';
 
 const TeacherPortalIndex = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/teacher-portal/one-time-code');
+  }, [router]);
+
   return null;
 };
 
 export default TeacherPortalIndex;
+
+export const getServerSideProps = withCommonServerSideProps();
