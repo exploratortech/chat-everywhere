e94cea6d4974231c3d738861f58c8e732f7835cb
1orzero <laokameng.6@gmail.com>
2024-04-05 17:49:13 +0800

    Refactor TagFilter component to use TagComponent and add resetTags functionality

--- components/TeacherPortal/ShareMessages/TagFilter.tsx
+++ components/TeacherPortal/ShareMessages/TagFilter.tsx
@@ -1,23 +1,18 @@
-import { IconTag } from '@tabler/icons-react';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Tag } from '@/types/tags';
 
+import * as TagComponent from '@/components/TeacherPortal/Tags/Tag';
 import useShareMessageFilterStore from '@/components/TeacherPortal/share-message-filter.store';
 import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 
 import { cn } from '@/lib/utils';
 
 const TagFilter = memo(({ tags }: { tags: Tag[] }) => {
   const { t } = useTranslation('model');
-  const { selectedTags, addTag, removeTag } = useShareMessageFilterStore();
+  const { selectedTags, addTag, removeTag, isNoTagsSelected, resetTags } =
+    useShareMessageFilterStore();
 
   const handleTagSelectionChange = (tag: Tag, checked: boolean) => {
     if (checked) {
@@ -28,46 +23,34 @@ const TagFilter = memo(({ tags }: { tags: Tag[] }) => {
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          className={cn(
-            'max-w-max px-2',
-            selectedTags.length > 0 ? 'bg-neutral-700' : '',
-          )}
-        >
-          <div className="flex items-center gap-2">
-            <IconTag className="w-4 h-4" />
-            {t('Tags')}
-          </div>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        align="start"
-        className="drop-shadow-2xl w-56 flex flex-col gap-4 p-4 border-0 shadow-lg bg-neutral-800"
+    <div className="relative flex flex-wrap gap-2">
+      <Button
+        variant={'link'}
+        onClick={() => {
+          resetTags();
+        }}
+        className={cn(
+          'absolute underline top-0 left-0 text-neutral-500 hover:text-neutral-400 translate-y-[-100%] p-0',
+          isNoTagsSelected() && 'invisible',
+        )}
       >
-        {tags.map((tag) => (
-          <div key={tag.id} className="flex items-center space-x-2">
-            <Checkbox
-              id={`dropdown-tag-${tag.id}`}
-              checked={selectedTags.some((t) => t.id === tag.id)}
-              onCheckedChange={(checked: boolean) => {
-                handleTagSelectionChange(tag, checked);
-              }}
-            ></Checkbox>
-            <label
-              htmlFor={`dropdown-tag-${tag.id}`}
-              className="cursor-pointer w-full text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-              {`${tag.name} ${
-                tag.message_count ? `(${tag.message_count})` : ''
-              }`}
-            </label>
-          </div>
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+        {`Clear ${selectedTags.length} selected tags`}
+      </Button>
+      {tags.map((tag) => (
+        <TagComponent.default
+          key={tag.id}
+          label={tag.name}
+          count={tag.message_count}
+          selected={selectedTags.some((t) => t.id === tag.id)}
+          onSelect={() =>
+            handleTagSelectionChange(
+              tag,
+              !selectedTags.some((t) => t.id === tag.id),
+            )
+          }
+        />
+      ))}
+    </div>
   );
 });
 
