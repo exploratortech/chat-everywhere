1b05d127d2694597d908ef499f317d4da032ac5b
1orzero <laokameng.6@gmail.com>
2024-04-05 17:49:07 +0800

    Refactor ShareMessages/Filter component

--- components/TeacherPortal/ShareMessages/Filter.tsx
+++ components/TeacherPortal/ShareMessages/Filter.tsx
@@ -2,26 +2,13 @@ import { memo } from 'react';
 
 import { Tag } from '@/types/tags';
 
-import { Button } from '@/components/ui/button';
-
-import useShareMessageFilterStore from '../share-message-filter.store';
 import SortBy from './SortBy';
 import TagFilter from './TagFilter';
 
 const Filter = memo(({ tags }: { tags: Tag[] }) => {
-  const { selectedTags, resetTags } = useShareMessageFilterStore();
   return (
-    <div className="flex justify-between gap-2 items-center">
+    <div className="flex justify-between gap-4 items-start">
       <TagFilter tags={tags} />
-      {selectedTags.length > 0 && (
-        <Button
-          variant={'link'}
-          onClick={() => resetTags()}
-          className="text-neutral-500 hover:text-neutral-400"
-        >
-          {`Clear ${selectedTags.length} selected tags`}
-        </Button>
-      )}
       <SortBy />
     </div>
   );
