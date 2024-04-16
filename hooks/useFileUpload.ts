daa390847dd1b7cb94603fd9e48da318227cb367
1orzero <laokameng.6@gmail.com>
2024-04-16 23:34:59 +0800

    Add access token to file upload request

--- hooks/useFileUpload.ts
+++ hooks/useFileUpload.ts
@@ -1,11 +1,17 @@
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+
 export function useFileUpload() {
+  const supabase = useSupabaseClient();
   return async (filename: string, file: File) => {
+    const accessToken = (await supabase.auth.getSession()).data.session
+      ?.access_token!;
     const result = await fetch(`/api/files/upload-url`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'access-token': accessToken,
       },
-      body: JSON.stringify({ file: filename }),
+      body: JSON.stringify({ fileName: filename }),
     });
     const { url, fields } = await result.json();
     const formData = new FormData();
