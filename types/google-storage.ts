b6a330417609b0111c686a37e89e3ba1b260adc1
1orzero <laokameng.6@gmail.com>
2024-04-27 09:52:38 +0800

    Add Google Storage types

---
+++ types/google-storage.ts
@@ -0,0 +1,24 @@
+interface Metadata {
+  'user-id': string;
+}
+
+export interface StorageObject {
+  kind: string;
+  id: string;
+  selfLink: string;
+  mediaLink: string;
+  name: string;
+  bucket: string;
+  generation: string;
+  metageneration: string;
+  contentType: string;
+  storageClass: string;
+  size: string;
+  md5Hash: string;
+  crc32c: string;
+  etag: string;
+  timeCreated: string;
+  updated: string;
+  timeStorageClassUpdated: string;
+  metadata: Metadata;
+}
