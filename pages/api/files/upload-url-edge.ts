77b7fda8a356e8e44407adc734e9ff5222d42eec
1orzero <laokameng.6@gmail.com>
2024-04-29 00:05:42 +0800

    Refactor Google Cloud Storage upload URL generation

--- pages/api/files/upload-url-edge.ts
+++ pages/api/files/upload-url-edge.ts
@@ -1,4 +1,3 @@
-// TODO: this is a WIP api route for Edge runtime
 import { createSignature } from '@/utils/server/google/signature';
 
 import dayjs from 'dayjs';
@@ -11,7 +10,6 @@ export const config = {
 };
 const BUCKET_NAME = process.env.GCP_CHAT_WITH_DOCUMENTS_BUCKET_NAME as string;
 const CLIENT_EMAIL = process.env.GCP_CLIENT_EMAIL as string;
-const PRIVATE_KEY = process.env.GCP_PRIVATE_KEY as string;
 
 export default async function handler(req: Request) {
   const { method } = req;
@@ -32,87 +30,95 @@ export default async function handler(req: Request) {
   const userProfile = { id: '98ba69f3-5648-4951-8308-128a90a6f770' };
   const folderPath = userProfile.id;
   const objectPath = `${folderPath}/${fileName}`;
+  const expiration = 3600;
 
-  // Construct the canonical request
-  const expiration = 3600; // URL valid for 1 hour
-  const canonicalRequest = `PUT\n/${BUCKET_NAME}/${objectPath}\n\nhost:storage.googleapis.com\ncontent-type:application/octet-stream\nx-goog-date:${dayjs()
-    .utc()
-    .format('YYYYMMDDTHHmmss[Z]')}\nx-goog-expires:${expiration}`;
+  const escapedObjectName = encodeURIComponent(objectPath);
+  const canonicalUri = `/${escapedObjectName}`;
 
-  // Convert the string to a Uint8Array
-  const encoder = new TextEncoder();
-  const encodedCanonicalRequest = encoder.encode(canonicalRequest);
+  const dateTimeNow = dayjs().utc();
+  const requestTimestamp = dateTimeNow.format('YYYYMMDDTHHmmss[Z]');
+  const dateStamp = dateTimeNow.format('YYYYMMDD');
 
-  const hashedCanonicalRequest = await crypto.subtle.digest(
-    'SHA-256',
-    encodedCanonicalRequest,
-  );
+  const clientEmail = CLIENT_EMAIL;
+  const credentialScope = `${dateStamp}/auto/storage/goog4_request`;
+  const credential = `${clientEmail}/${credentialScope}`;
 
-  // Create the string-to-sign
-  const credentialScope = `${dayjs().format(
-    'YYYYMMDD',
-  )}/auto/storage/goog4_request`;
-  const stringToSign = `GOOG4-RSA-SHA256\n${dayjs()
-    .utc()
-    .format(
-      'YYYYMMDDTHHmmss[Z]',
-    )}\n${credentialScope}\n${hashedCanonicalRequest}`;
+  const host = `${BUCKET_NAME}.storage.googleapis.com`;
+  const headers = {
+    host: host,
+  };
+  const signedHeaders = Object.keys(headers)
+    .map((key) => key.toLowerCase())
+    .sort()
+    .join(';');
+  const canonicalHeaders =
+    Object.keys(headers)
+      .map(
+        (key) =>
+          `${key.toLowerCase()}:${headers[
+            key as keyof typeof headers
+          ].toLowerCase()}`,
+      )
+      .join('\n') + '\n';
+  const queryParams = {
+    'response-content-disposition': 'attachment; filename=' + fileName,
+    'X-Goog-Algorithm': 'GOOG4-RSA-SHA256',
+    'X-Goog-Credential': credential,
+    'X-Goog-Date': requestTimestamp,
+    'X-Goog-Expires': expiration,
+    'X-Goog-SignedHeaders': signedHeaders,
+  };
+  const canonicalQueryString = Object.keys(queryParams)
+    .sort()
+    .map(
+      (key) =>
+        `${key}=${encodeURIComponent(
+          queryParams[key as keyof typeof queryParams],
+        )}`,
+    )
+    .join('&');
+  const canonicalRequest = [
+    'PUT',
+    canonicalUri,
+    canonicalQueryString,
+    canonicalHeaders,
+    signedHeaders,
+    'UNSIGNED-PAYLOAD',
+  ].join('\n');
+  async function hashCanonicalRequest(input: string): Promise<string> {
+    const encoder = new TextEncoder();
+    const encoded = encoder.encode(input);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', encoded);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, '0'))
+      .join('');
+    return hashHex;
+  }
 
-  // Generate the signature
+  const canonicalRequestHash = await hashCanonicalRequest(canonicalRequest);
+
+  const stringToSign = [
+    'GOOG4-RSA-SHA256',
+    requestTimestamp,
+    credentialScope,
+    canonicalRequestHash,
+  ].join('\n');
   const signature = await createSignature(stringToSign);
 
-  const algorithm = 'GOOG4-RSA-SHA256';
-  const credential = `${CLIENT_EMAIL}/${dayjs().format(
-    'YYYYMMDD',
-  )}/auto/storage/goog4_request`;
-  const date = dayjs().utc().format('YYYYMMDDTHHmmss[Z]');
+  const schemeAndHost = `https://${host}`;
 
-  const generatePolicyDocumentHash = () => {
-    const expirationDate = dayjs().add(expiration, 'seconds').toISOString();
-
-    const policyDocument = {
-      expiration: expirationDate,
-      conditions: [
-        { bucket: BUCKET_NAME },
-        ['starts-with', '$key', `${objectPath}`],
-        { acl: 'bucket-owner-full-control' },
-        ['content-length-range', 0, 10485760], // 10 MB max file size
-        { 'x-goog-algorithm': algorithm },
-        {
-          'x-goog-credential': credential,
-        },
-        { 'x-goog-date': date },
-      ],
-    };
-
-    const stringifiedPolicy = JSON.stringify(policyDocument);
-    const encodedPolicy = btoa(unescape(encodeURIComponent(stringifiedPolicy))); // Properly handle Unicode characters
-    return encodedPolicy;
-  };
-
-  const fields = {
-    'x-goog-algorithm': algorithm,
-    'x-goog-credential': credential,
-    'x-goog-date': date,
-    'x-goog-signature': signature,
-  };
-  // Construct the signed URL
-  // const queryParams = new URLSearchParams(fields);
-
-  // const signedUrl = `https://storage.googleapis.com/${BUCKET_NAME}/${objectPath}?${queryParams}`;
-  const url = `https://storage.googleapis.com/${BUCKET_NAME}/`;
+  const signedUrl = `${schemeAndHost}${canonicalUri}?${canonicalQueryString}&x-goog-signature=${signature}`;
 
   return new Response(
     JSON.stringify({
-      url: url,
-      fields: {
-        ...fields,
-        key: objectPath,
-        policy: generatePolicyDocumentHash(),
-        acl: 'bucket-owner-full-control',
-      },
+      url: signedUrl,
+      stringToSign,
+      canonicalRequest,
+      signature,
     }),
     {
+      headers: headers,
       status: 200,
     },
   );
