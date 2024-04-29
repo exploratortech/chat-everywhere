f8885aa92e5e62aa5ef0401c0d60b23ff32935e5
1orzero <laokameng.6@gmail.com>
2024-04-29 09:41:19 +0800

    Refactor useFileUpload function to separate upload logic into a separate function

--- hooks/useFileUpload.ts
+++ hooks/useFileUpload.ts
@@ -1,29 +1,36 @@
-import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import {
+  SupabaseClient,
+  useSupabaseClient,
+} from '@supabase/auth-helpers-react';
 
 export function useFileUpload() {
   const supabase = useSupabaseClient();
-  return async (filename: string, file: File) => {
-    const accessToken = (await supabase.auth.getSession()).data.session
-      ?.access_token!;
-    // DOING: WIP
-    const result = await fetch(`/api/files/upload-url-edge`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'access-token': accessToken,
-      },
-      body: JSON.stringify({ fileName: filename }),
-    });
-    const { url, fields } = await result.json();
-    const formData = new FormData();
-    Object.entries({ ...fields, file }).forEach(([key, value]) => {
-      formData.append(key, value as string | Blob);
-    });
-    const upload = await fetch(url, {
-      // DOING: WIP
-      method: 'PUT',
-      body: formData,
-    });
-    return upload.ok;
-  };
+  return uploadFile.bind(null, supabase);
+}
+
+async function uploadFile(
+  supabase: SupabaseClient<any, 'public', any>,
+  filename: string,
+  file: File,
+) {
+  const accessToken = (await supabase.auth.getSession()).data.session
+    ?.access_token!;
+  const result = await fetch(`/api/files/upload-url`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'access-token': accessToken,
+    },
+    body: JSON.stringify({ fileName: filename }),
+  });
+  const { url, fields } = await result.json();
+  const formData = new FormData();
+  Object.entries({ ...fields, file }).forEach(([key, value]) => {
+    formData.append(key, value as string | Blob);
+  });
+  const upload = await fetch(url, {
+    method: 'POST',
+    body: formData,
+  });
+  return upload.ok;
 }
