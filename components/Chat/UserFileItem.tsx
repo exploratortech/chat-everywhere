51a649f7498d7b29d7c2702708a3daed4a43ebcb
1orzero <laokameng.6@gmail.com>
2024-05-01 21:59:33 +0800

    Add error tooltip for user file item

--- components/Chat/UserFileItem.tsx
+++ components/Chat/UserFileItem.tsx
@@ -5,6 +5,13 @@ import { useFetchFileList } from '@/hooks/file/useFetchFileList';
 
 import { UserFile } from '@/types/UserFile';
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
 import HomeContext from '../home/home.context';
 import UserFileItemIcon from './UserFileItemIcon';
 
@@ -25,23 +32,41 @@ const UserFileItem: React.FC<UserFileItemProps> = ({ file, onRemove }) => {
     () => files?.some((f) => f.id === file.id),
     [files, file.id],
   );
-  const fileCannotBeUsed = useMemo(
-    () => !isUltraUser || !fileExists,
-    [isUltraUser, fileExists],
-  );
+  const errorTooltipMessage = useMemo(() => {
+    if (!isUltraUser) {
+      return 'You are not an Ultra user, please upgrade to Ultra to use this Gemini Chat';
+    }
+    if (!fileExists) {
+      return 'This file does not exist, it will be deleted from the chat';
+    }
+    return '';
+  }, [isUltraUser, fileExists]);
+
+  if (errorTooltipMessage) {
+    return (
+      <ErrorTooltip message={errorTooltipMessage}>
+        <div
+          className={cn(
+            'group p-2 border select-none flex gap-2 items-center rounded-md bg-red-100 border-red-500 text-black',
+          )}
+        >
+          <IconAlertCircleFilled className="text-red-500" />
+          {file.filename}
+          <div className="h-full cursor-pointer group-hover:visible invisible flex items-center">
+            <IconX className="" size={14} onClick={() => onRemove(file)} />
+          </div>
+        </div>
+      </ErrorTooltip>
+    );
+  }
+
   return (
     <div
       className={cn(
         'group p-2 border select-none flex gap-2 items-center border-gray-500 rounded-md',
-        fileCannotBeUsed ? 'bg-red-100 border-red-500 text-black' : '',
       )}
     >
-      {fileCannotBeUsed ? (
-        <IconAlertCircleFilled className="text-red-500" />
-      ) : (
-        <UserFileItemIcon fileType={file.filetype} />
-      )}
-
+      <UserFileItemIcon fileType={file.filetype} />
       {file.filename}
       <div className="h-full cursor-pointer group-hover:visible invisible flex items-center">
         <IconX className="" size={14} onClick={() => onRemove(file)} />
@@ -51,3 +76,20 @@ const UserFileItem: React.FC<UserFileItemProps> = ({ file, onRemove }) => {
 };
 
 export default UserFileItem;
+
+function ErrorTooltip({
+  message,
+  children,
+}: {
+  message: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent side="bottom">{message}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
