a3853cd3b084d2d8d48a157715fa12bb87ce61be
Rushi Patel <mail.rushipatel@gmail.com>
2024-05-01 16:27:59 -0400

    make settings params optional

--- types/teacher-settings.ts
+++ types/teacher-settings.ts
@@ -1,7 +1,8 @@
 export interface TeacherSettings {
-  allow_student_use_line: boolean;
-  hidden_chateverywhere_default_character_prompt: boolean;
+  allow_student_use_line?: boolean;
+  hidden_chateverywhere_default_character_prompt?: boolean;
 }
 export interface TeacherSettingsInPortal extends TeacherSettings {
-  should_clear_conversations_on_logout: boolean;
+  should_clear_conversations_on_logout?: boolean;
+  items_per_page?: number;
 }
