5239559364562c6e183adeee508eaa3c20920ff2
1orzero <laokameng.6@gmail.com>
2024-05-02 11:24:33 +0800

    Add loading bar to file upload

--- hooks/file/useFileUpload.ts
+++ hooks/file/useFileUpload.ts
@@ -6,6 +6,8 @@ import { useTranslation } from 'react-i18next';
 
 import HomeContext from '@/components/home/home.context';
 
+import useHomeLoadingBar from '../useHomeLoadingBar';
+
 export function useFileUpload() {
   const supabase = useSupabaseClient();
   const { t: commonT } = useTranslation('common');
@@ -14,54 +16,56 @@ export function useFileUpload() {
     state: { user },
   } = useContext(HomeContext);
   const [uploadProgress, setUploadProgress] = useState<number | null>(null);
+  const { withLoading } = useHomeLoadingBar();
 
   const uploadFileMutation = useMutation(
-    async ({ filename, file }: { filename: string; file: File }) => {
-      const accessToken = (await supabase.auth.getSession()).data.session
-        ?.access_token!;
-      const result = await fetch(`/api/files/upload-url`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'access-token': accessToken,
-        },
-        body: JSON.stringify({ fileName: filename }),
-      });
-      const { url, fields } = await result.json();
-
-      return new Promise((resolve, reject) => {
-        const formData = new FormData();
-        Object.entries({ ...fields, file }).forEach(([key, value]) => {
-          formData.append(key, value as string | Blob);
+    async ({ filename, file }: { filename: string; file: File }) =>
+      withLoading(async () => {
+        const accessToken = (await supabase.auth.getSession()).data.session
+          ?.access_token!;
+        const result = await fetch(`/api/files/upload-url`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'access-token': accessToken,
+          },
+          body: JSON.stringify({ fileName: filename }),
         });
+        const { url, fields } = await result.json();
 
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', url);
+        return new Promise((resolve, reject) => {
+          const formData = new FormData();
+          Object.entries({ ...fields, file }).forEach(([key, value]) => {
+            formData.append(key, value as string | Blob);
+          });
 
-        xhr.onload = () => {
-          if (xhr.status >= 200 && xhr.status < 300) {
-            resolve(true);
-          } else {
-            reject(xhr.statusText);
-          }
-        };
-        xhr.onerror = () => reject(xhr.statusText);
+          const xhr = new XMLHttpRequest();
+          xhr.open('POST', url);
 
-        // Progress listener
-        xhr.upload.onprogress = (event) => {
-          if (event.lengthComputable) {
-            const percentage = (event.loaded / event.total) * 100;
-            setUploadProgress(Math.round(percentage));
-          }
-        };
+          xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+              resolve(true);
+            } else {
+              reject(xhr.statusText);
+            }
+          };
+          xhr.onerror = () => reject(xhr.statusText);
 
-        xhr.onloadend = () => {
-          setUploadProgress(null);
-        };
+          // Progress listener
+          xhr.upload.onprogress = (event) => {
+            if (event.lengthComputable) {
+              const percentage = (event.loaded / event.total) * 100;
+              setUploadProgress(Math.round(percentage));
+            }
+          };
 
-        xhr.send(formData);
-      });
-    },
+          xhr.onloadend = () => {
+            setUploadProgress(null);
+          };
+
+          xhr.send(formData);
+        });
+      }),
     {
       onSuccess: () => {
         toast.success(commonT('File uploaded successfully'));
