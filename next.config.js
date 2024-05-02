0c7cf5760e14799950161d3898195a459aa52be5
Jack Wu <jackwu833016@gmail.com>
2024-05-02 12:06:53 -0400

    Experiment with output file tracing on Vercel

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     outputFileTracingIncludes: {
-      '/api/**/*': ['../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm'],
+      '/api/v2/thread-handler': ['../../node_modules/@dqbd/tiktoken/lite/tiktoken_bg.wasm'],
     },
   },
 
