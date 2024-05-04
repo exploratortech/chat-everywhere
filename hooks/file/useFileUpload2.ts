a9576f082e1ea1f0e5c9c6892cf1055ec646382c
1orzero <laokameng.6@gmail.com>
2024-05-04 11:55:32 +0800

    Using the Node.js SDK to generate the upload URL temporarily

---
+++ hooks/file/useFileUpload2.ts
@@ -0,0 +1,95 @@
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useContext, useState } from 'react';
+import toast from 'react-hot-toast';
+import { useTranslation } from 'react-i18next';
+
+import HomeContext from '@/components/home/home.context';
+
+import useHomeLoadingBar from '../useHomeLoadingBar';
+
+export function useFileUpload() {
+  const supabase = useSupabaseClient();
+  const { t: commonT } = useTranslation('common');
+  const queryClient = useQueryClient();
+  const {
+    state: { user },
+  } = useContext(HomeContext);
+  const [uploadProgress, setUploadProgress] = useState<number | null>(null);
+  const { withLoading } = useHomeLoadingBar();
+
+  const uploadFileMutation = useMutation(
+    async ({ filename, file }: { filename: string; file: File }) =>
+      withLoading(async () => {
+        const accessToken = (await supabase.auth.getSession()).data.session
+          ?.access_token!;
+        const result = await fetch(`/api/files/upload-url-2`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'access-token': accessToken,
+          },
+          body: JSON.stringify({
+            fileName: filename,
+            fileMimeType: file.type,
+          }),
+        });
+        const { url, headers, fields } = await result.json();
+
+        return new Promise((resolve, reject) => {
+          const formData = new FormData();
+
+          if (fields) {
+            Object.entries({ ...fields }).forEach(([key, value]) => {
+              formData.append(key, value as string | Blob);
+            });
+          }
+          formData.append('file', file);
+          const xhr = new XMLHttpRequest();
+          xhr.open('POST', url);
+          if (headers) {
+            for (const [key, value] of Object.entries(headers)) {
+              if (key === 'host') continue;
+              xhr.setRequestHeader(`${key}`, value as string);
+            }
+          }
+
+          xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+              resolve(true);
+            } else {
+              reject(xhr.statusText);
+            }
+          };
+          xhr.onerror = () => reject(xhr.statusText);
+
+          // Progress listener
+          xhr.upload.onprogress = (event) => {
+            if (event.lengthComputable) {
+              const percentage = (event.loaded / event.total) * 100;
+              setUploadProgress(Math.round(percentage));
+            }
+          };
+
+          xhr.onloadend = () => {
+            setUploadProgress(null);
+          };
+
+          xhr.send(formData);
+        });
+      }),
+    {
+      onSuccess: () => {
+        toast.success(commonT('File uploaded successfully'));
+      },
+      onError: () => {
+        toast.error(commonT('File upload failed'));
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries(['gcp-files', user?.id]);
+      },
+    },
+  );
+
+  return { uploadFileMutation, uploadProgress };
+}
---
+++ pages/api/files/upload-url-2.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import {
+  fetchUserProfileWithAccessTokenServerless,
+  unauthorizedResponse,
+} from '@/utils/server/auth';
+import { getBucket } from '@/utils/server/gcpBucket';
+
+import { v4 as uuidv4 } from 'uuid';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { body, method } = req;
+  if (method !== 'POST') {
+    return res.status(405).json('Method not allowed');
+  }
+  const fileName = body.fileName;
+
+  if (!fileName) {
+    return res.status(400).json('File name is required');
+  }
+
+  const userProfile = await fetchUserProfileWithAccessTokenServerless(req);
+  if (!userProfile || !userProfile.isTeacherAccount)
+    return unauthorizedResponse;
+  const folderPath = userProfile.id;
+  const randomUUID = uuidv4();
+  const fileWithPath = `${folderPath}/${fileName}_${randomUUID}`;
+
+  const bucket = await getBucket();
+  const file = bucket.file(fileWithPath);
+
+  const options = {
+    expires: Date.now() + 5 * 60 * 1000,
+    fields: {
+      'x-goog-meta-user-id': userProfile.id,
+      'x-goog-meta-file-name': fileName,
+    },
+  };
+  const [response] = await file.generateSignedPostPolicyV4(options);
+  return res.status(200).json(response);
+}
