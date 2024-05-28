a1ac3a1f2576a7a4af0b4d0522dd9a955db15b3a
1orzero <laokameng.6@gmail.com>
2024-05-28 16:56:50 +0800

    feat: Add enums for PaidPlan and TopUpRequest

---
+++ types/paid_plan.ts
@@ -0,0 +1,11 @@
+export enum PaidPlan {
+  ProMonthly = 'pro-monthly',
+  ProOneTime = 'pro-one-time',
+  UltraMonthly = 'ultra-monthly',
+  UltraOneTime = 'ultra-one-time',
+}
+
+export enum TopUpRequest {
+  ImageCredit = 'image-credit',
+  GPT4Credit = 'gpt4-credit',
+}
