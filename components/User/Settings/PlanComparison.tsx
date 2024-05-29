ce364279b913f6bfcf1204d8cefb55da89d937ad
1orzero <laokameng.6@gmail.com>
2024-05-29 11:49:13 +0800

    refactor: Update payment links for Pro and Ultra plans
    Update the payment links for the Pro and Ultra plans in the stripe_config.ts file. Add separate links for the monthly plans in USD and TWD currencies. Also, add links for the yearly Ultra plan in USD and TWD currencies. Remove the unused constant in the file.

--- components/User/Settings/PlanComparison.tsx
+++ components/User/Settings/PlanComparison.tsx
@@ -1,59 +1,26 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 
 import {
   OrderedSubscriptionPlans,
-  PRO_PLAN_PAYMENT_LINK,
-  ULTRA_PLAN_PAYMENT_LINK,
+  PRO_MONTHLY_PLAN_PAYMENT_LINK_TWD,
+  PRO_MONTHLY_PLAN_PAYMENT_LINK_USD,
+  ULTRA_MONTHLY_PLAN_PAYMENT_LINK_TWD,
+  ULTRA_MONTHLY_PLAN_PAYMENT_LINK_USD,
+  ULTRA_YEARLY_PLAN_PAYMENT_LINK_TWD,
+  ULTRA_YEARLY_PLAN_PAYMENT_LINK_USD,
 } from '@/utils/app/const';
 import { trackEvent } from '@/utils/app/eventTracking';
 import { FeatureItem, PlanDetail } from '@/utils/app/ui';
 
 import { User } from '@/types/user';
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 import dayjs from 'dayjs';
 
 const PlanComparison = ({ user }: { user: User | null }) => {
-  const { t } = useTranslation('model');
-
-  const upgradeLinkOnClick = (upgradePlan: 'pro' | 'ultra') => {
-    let paymentLink = '';
-    if (upgradePlan === 'pro') {
-      paymentLink = PRO_PLAN_PAYMENT_LINK;
-    } else if (upgradePlan === 'ultra') {
-      paymentLink = ULTRA_PLAN_PAYMENT_LINK;
-    }
-
-    const userEmail = user?.email;
-    const userId = user?.id;
-
-    trackEvent('Upgrade button clicked');
-
-    if (!user) {
-      toast.error('Please sign-up before upgrading to pro plan');
-    } else {
-      window.open(
-        `${paymentLink}?prefilled_email=${userEmail}&client_reference_id=${userId}`,
-        '_blank',
-      );
-    }
-  };
-
-  const showUpgradeToPro = useMemo(() => {
-    if (!user) return true;
-    const userPlanIndex = OrderedSubscriptionPlans.indexOf(user.plan);
-    const proPlanIndex = OrderedSubscriptionPlans.indexOf('pro');
-    return userPlanIndex < proPlanIndex;
-  }, [user]);
-
-  const showUpgradeToUltra = useMemo(() => {
-    if (!user) return true;
-    const userPlanIndex = OrderedSubscriptionPlans.indexOf(user.plan);
-    const ultraPlanIndex = OrderedSubscriptionPlans.indexOf('ultra');
-    return userPlanIndex < ultraPlanIndex;
-  }, [user]);
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-[1fr_1fr] justify-center gap-3 mb-3">
       {/* Free Plan */}
@@ -64,55 +31,11 @@ const PlanComparison = ({ user }: { user: User | null }) => {
       {/* Pro Plan */}
       <div className="flex flex-col w-full col-start-1 row-span-1 border rounded-lg p-4">
         <ProPlanContent user={user} />
-
-        {/* Upgrade button */}
-        {showUpgradeToPro && (
-          <div className="flex flex-col">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              onClick={() => upgradeLinkOnClick('pro')}
-              className="px-4 py-2 border rounded-lg bg-white shadow border-none text-white font-semibold focus:outline-none mt-4 text-center text-sm cursor-pointer bg-gradient-to-r from-[#fd68a6] to-[#6c62f7]"
-            >
-              {t('Upgrade')}
-            </a>
-            <p className="text-xs text-neutral-400 mt-2">
-              {t('No Strings Attached - Cancel Anytime!')}
-            </p>
-          </div>
-        )}
-
-        {(user?.plan === 'pro' || user?.plan === 'ultra') &&
-          user.proPlanExpirationDate && (
-            <PlanExpirationDate expirationDate={user.proPlanExpirationDate} />
-          )}
       </div>
 
       {/* Ultra Plan */}
       <div className="flex flex-col w-full col-start-1 row-start-auto md:row-start-1 md:col-start-2 row-span-2 border rounded-lg p-4">
         <UltraPlanContent user={user} />
-
-        {/* Upgrade button */}
-        {showUpgradeToUltra && (
-          <div className="flex flex-col">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              onClick={() => upgradeLinkOnClick('ultra')}
-              className="px-4 py-2 border rounded-lg bg-white shadow border-none text-white font-semibold focus:outline-none mt-4 text-center text-sm cursor-pointer bg-gradient-to-r from-[#fd68a6] to-[#6c62f7]"
-            >
-              {t('Upgrade')}
-            </a>
-            <p className="text-xs text-neutral-400 mt-2">
-              {t('No Strings Attached - Cancel Anytime!')}
-            </p>
-          </div>
-        )}
-
-        {(user?.plan === 'pro' || user?.plan === 'ultra') &&
-          user.proPlanExpirationDate && (
-            <PlanExpirationDate expirationDate={user.proPlanExpirationDate} />
-          )}
       </div>
     </div>
   );
@@ -151,7 +74,34 @@ const FreePlanContent = ({ user }: { user: User | null }) => {
   );
 };
 const ProPlanContent = ({ user }: { user: User | null }) => {
-  const { t } = useTranslation('model');
+  const { t, i18n } = useTranslation('model');
+  const showUpgradeToPro = useMemo(() => {
+    if (!user) return true;
+    const userPlanIndex = OrderedSubscriptionPlans.indexOf(user.plan);
+    const proPlanIndex = OrderedSubscriptionPlans.indexOf('pro');
+    return userPlanIndex < proPlanIndex;
+  }, [user]);
+  const upgradeLinkOnClick = () => {
+    const paymentLink =
+      i18n.language === 'zh-Hant' || i18n.language === 'zh'
+        ? PRO_MONTHLY_PLAN_PAYMENT_LINK_TWD
+        : PRO_MONTHLY_PLAN_PAYMENT_LINK_USD;
+
+    const userEmail = user?.email;
+    const userId = user?.id;
+
+    trackEvent('Upgrade button clicked');
+
+    if (!user) {
+      toast.error(t('Please sign-up before upgrading to paid plan'));
+    } else {
+      window.open(
+        `${paymentLink}?prefilled_email=${userEmail}&client_reference_id=${userId}`,
+        '_blank',
+      );
+    }
+  };
+
   return (
     <>
       <div className="flex flex-row items-center justify-between gap-2">
@@ -168,7 +118,7 @@ const ProPlanContent = ({ user }: { user: User | null }) => {
         </span>
         {user?.plan === 'pro' && <CurrentTag />}
       </div>
-      <span className="text-sm mb-2">{t('USD$9.99 / month')}</span>
+      <ProPlanPrice />
       <div className="text-xs leading-5">
         <FeatureItem featureName={t('Everything in free plan')} />
         <FeatureItem featureName={t('Priority response time')} />
@@ -176,12 +126,72 @@ const ProPlanContent = ({ user }: { user: User | null }) => {
           <FeatureItem key={index} featureName={t(feature)} />
         ))}
       </div>
+      {/* Upgrade button */}
+      {showUpgradeToPro && (
+        <div className="flex flex-col">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            onClick={upgradeLinkOnClick}
+            className="px-4 py-2 border rounded-lg bg-white shadow border-none text-white font-semibold focus:outline-none mt-4 text-center text-sm cursor-pointer bg-gradient-to-r from-[#fd68a6] to-[#6c62f7]"
+          >
+            {t('Upgrade')}
+          </a>
+          <p className="text-xs text-neutral-400 mt-2">
+            {t('No Strings Attached - Cancel Anytime!')}
+          </p>
+        </div>
+      )}
+
+      {(user?.plan === 'pro' || user?.plan === 'ultra') &&
+        user.proPlanExpirationDate && (
+          <PlanExpirationDate expirationDate={user.proPlanExpirationDate} />
+        )}
     </>
   );
 };
 
 const UltraPlanContent = ({ user }: { user: User | null }) => {
-  const { t } = useTranslation('model');
+  const { t, i18n } = useTranslation('model');
+  const [priceType, setPriceType] = useState<'monthly' | 'yearly'>('monthly');
+  const showUpgradeToUltra = useMemo(() => {
+    if (!user) return true;
+    const userPlanIndex = OrderedSubscriptionPlans.indexOf(user.plan);
+    const ultraPlanIndex = OrderedSubscriptionPlans.indexOf('ultra');
+    return userPlanIndex < ultraPlanIndex;
+  }, [user]);
+
+  const upgradeLinkOnClick = () => {
+    let paymentLink = ULTRA_MONTHLY_PLAN_PAYMENT_LINK_USD;
+    if (priceType === 'monthly') {
+      if (i18n.language === 'zh-Hant' || i18n.language === 'zh') {
+        paymentLink = ULTRA_MONTHLY_PLAN_PAYMENT_LINK_TWD;
+      } else {
+        paymentLink = ULTRA_MONTHLY_PLAN_PAYMENT_LINK_USD;
+      }
+    } else {
+      if (i18n.language === 'zh-Hant' || i18n.language === 'zh') {
+        paymentLink = ULTRA_YEARLY_PLAN_PAYMENT_LINK_TWD;
+      } else {
+        paymentLink = ULTRA_YEARLY_PLAN_PAYMENT_LINK_USD;
+      }
+    }
+
+    const userEmail = user?.email;
+    const userId = user?.id;
+
+    trackEvent('Upgrade button clicked');
+
+    if (!user) {
+      toast.error(t('Please sign-up before upgrading to paid plan'));
+    } else {
+      window.open(
+        `${paymentLink}?prefilled_email=${userEmail}&client_reference_id=${userId}`,
+        '_blank',
+      );
+    }
+  };
+
   return (
     <>
       <div className="flex flex-row items-center justify-between gap-2">
@@ -198,7 +208,7 @@ const UltraPlanContent = ({ user }: { user: User | null }) => {
         </span>
         {user?.plan === 'ultra' && <CurrentTag />}
       </div>
-      <span className="text-sm mb-2">{t('USD$19.99 / month')}</span>
+      <UltraPlanPrice setPriceType={setPriceType} />
 
       <div className="text-xs leading-5">
         <FeatureItem featureName={t('Everything in free plan')} />
@@ -207,6 +217,27 @@ const UltraPlanContent = ({ user }: { user: User | null }) => {
           <FeatureItem key={index} featureName={t(feature)} />
         ))}
       </div>
+      {/* Upgrade button */}
+      {showUpgradeToUltra && (
+        <div className="flex flex-col">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            onClick={upgradeLinkOnClick}
+            className="px-4 py-2 border rounded-lg bg-white shadow border-none text-white font-semibold focus:outline-none mt-4 text-center text-sm cursor-pointer bg-gradient-to-r from-[#fd68a6] to-[#6c62f7]"
+          >
+            {t('Upgrade')}
+          </a>
+          <p className="text-xs text-neutral-400 mt-2">
+            {t('No Strings Attached - Cancel Anytime!')}
+          </p>
+        </div>
+      )}
+
+      {(user?.plan === 'pro' || user?.plan === 'ultra') &&
+        user.proPlanExpirationDate && (
+          <PlanExpirationDate expirationDate={user.proPlanExpirationDate} />
+        )}
     </>
   );
 };
@@ -216,5 +247,64 @@ const CurrentTag = () => {
     <span className="h-max bg-neutral-600 text-neutral-400 text-[10px]  font-medium mr-2 px-2 py-[.5px] rounded w-max">
       CURRENT PLAN
     </span>
+  );
+};
+
+const ProPlanPrice = () => {
+  const { i18n } = useTranslation('model');
+
+  switch (i18n.language) {
+    case 'zh-Hant':
+    case 'zh':
+      return <span className="text-sm mb-2">{'TWD$249.99 / month'}</span>;
+    default:
+      return <span className="text-sm mb-2">{'USD$9.99 / month'}</span>;
+  }
+};
+
+const UltraPlanPrice = ({
+  setPriceType,
+}: {
+  setPriceType: (type: 'monthly' | 'yearly') => void;
+}) => {
+  const { i18n } = useTranslation('model');
+
+  return (
+    <Tabs defaultValue="monthly" className="mt-2 mb-4 w-full">
+      <TabsList className="w-full">
+        <TabsTrigger
+          value="monthly"
+          className="w-full"
+          onClick={() => {
+            setPriceType('monthly');
+          }}
+        >
+          MONTHLY
+        </TabsTrigger>
+        <TabsTrigger
+          value="yearly"
+          className="w-full"
+          onClick={() => {
+            setPriceType('yearly');
+          }}
+        >
+          YEARLY
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="monthly">
+        {i18n.language === 'zh-Hant' || i18n.language === 'zh' ? (
+          <span className="text-sm mb-2">{'TWD$880 / month'}</span>
+        ) : (
+          <span className="text-sm mb-2">{'USD$29.99 / month'}</span>
+        )}
+      </TabsContent>
+      <TabsContent value="yearly">
+        {i18n.language === 'zh-Hant' || i18n.language === 'zh' ? (
+          <span className="text-sm mb-2">{'TWD$8800 / year'}</span>
+        ) : (
+          <span className="text-sm mb-2">{'USD$279.99 / year'}</span>
+        )}
+      </TabsContent>
+    </Tabs>
   );
 };
--- utils/app/stripe_config.ts
+++ utils/app/stripe_config.ts
@@ -10,6 +10,10 @@ export const STRIPE_PLAN_CODE_MONTHLY_PRO_PLAN_SUBSCRIPTION =
 export const STRIPE_PLAN_CODE_MONTHLY_ULTRA_PLAN_SUBSCRIPTION =
   'monthly_ultra_plan_subscription';
 
+// STRIPE YEARLY PLAN CODE
+export const STRIPE_PLAN_CODE_YEARLY_ULTRA_PLAN_SUBSCRIPTION =
+  'yearly_ultra_plan_subscription';
+
 // STRIPE ONE TIME PLAN CODE
 export const STRIPE_PLAN_CODE_ONE_TIME_PRO_PLAN_FOR_1_MONTH =
   'one_time_pro_plan_for_1_month';
@@ -18,16 +22,42 @@ export const STRIPE_PLAN_CODE_ONE_TIME_PRO_PLAN_FOR_1_MONTH =
 export const STRIPE_PLAN_CODE_ONE_TIME_ULTRA_PLAN_FOR_1_MONTH =
   'one_time_ultra_plan_for_1_month';
 
-export const PRO_PLAN_PAYMENT_LINK =
+// =========== PRO PLAN LINKS ===========
+// PRO MONTHLY PLAN
+export const PRO_MONTHLY_PLAN_PAYMENT_LINK_USD =
   process.env.NEXT_PUBLIC_ENV === 'production'
     ? 'https://buy.stripe.com/8wM8Av2DM0u99fWfZ1'
     : 'https://buy.stripe.com/test_4gw4hLcvq52Odt6fYY';
 
-export const ULTRA_PLAN_PAYMENT_LINK =
+export const PRO_MONTHLY_PLAN_PAYMENT_LINK_TWD =
   process.env.NEXT_PUBLIC_ENV === 'production'
-    ? 'https://buy.stripe.com/8wM8Av2DM0u99fWfZ1' // TODO: Update the production link
+    ? '' // TODO: Update the production link
+    : ''; // TODO: Update the test link
+
+// =========== ULTRA PLAN LINKS ===========
+// ULTRA MONTHLY PLAN
+export const ULTRA_MONTHLY_PLAN_PAYMENT_LINK_USD =
+  process.env.NEXT_PUBLIC_ENV === 'production'
+    ? '' // TODO: Update the production link
+    : '';
+
+export const ULTRA_MONTHLY_PLAN_PAYMENT_LINK_TWD =
+  process.env.NEXT_PUBLIC_ENV === 'production'
+    ? '' // TODO: Update the production link
     : 'https://buy.stripe.com/test_00gcOhbrmgLwbkYdR0';
 
+// ULTRA YEARLY PLAN
+export const ULTRA_YEARLY_PLAN_PAYMENT_LINK_USD =
+  process.env.NEXT_PUBLIC_ENV === 'production'
+    ? '' // TODO: Update the production link
+    : ''; // TODO: Update the test link
+
+export const ULTRA_YEARLY_PLAN_PAYMENT_LINK_TWD =
+  process.env.NEXT_PUBLIC_ENV === 'production'
+    ? '' // TODO: Update the production link
+    : ''; // TODO: Update the test link
+
+// =========== TOP UP LINKS ===========
 export const GPT4_CREDIT_PURCHASE_LINKS = {
   '50': 'https://buy.stripe.com/28o03Z0vE3Glak09AJ',
   '150': 'https://buy.stripe.com/cN2dUP6U2dgV0JqcMW',
@@ -38,6 +68,7 @@ export const AI_IMAGE_CREDIT_PURCHASE_LINKS = {
   '500': 'https://buy.stripe.com/8wMg2XcemccR2Ry8wQ',
 };
 
+// =========== V2 UPGRADE LINKS ===========
 export const V2_CHAT_UPGRADE_LINK =
   process.env.NEXT_PUBLIC_ENV === 'production'
     ? 'https://buy.stripe.com/4gw9Ez6U2gt71NudRd'
