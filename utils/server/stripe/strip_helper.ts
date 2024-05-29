3f1a88a1d8d575b6119c9c31586a662a721ce3b8
1orzero <laokameng.6@gmail.com>
2024-05-29 12:08:28 +0800

    Refactor: Remove the updateUserAccount and make the function more atomic

--- utils/server/stripe/strip_helper.ts
+++ utils/server/stripe/strip_helper.ts
@@ -1,4 +1,8 @@
+import { getAdminSupabaseClient } from '../supabase';
+
 import Stripe from 'stripe';
+
+const supabase = getAdminSupabaseClient();
 
 export default async function getCustomerEmailByCustomerID(
   customerID: string,
@@ -21,4 +25,130 @@ export default async function getCustomerEmailByCustomerID(
   } catch (e) {
     throw new Error(`getCustomerEmailByCustomerID failed: ${e}`);
   }
+}
+
+export async function updateUserAccountById({
+  userId,
+  plan,
+  stripeSubscriptionId,
+  proPlanExpirationDate,
+}: {
+  userId: string;
+  plan?: string;
+  stripeSubscriptionId?: string;
+  proPlanExpirationDate?: Date;
+}) {
+  // Update user account by User ID
+  const { data: userProfile } = await supabase
+    .from('profiles')
+    .select('plan')
+    .eq('id', userId)
+    .single();
+
+  if (userProfile?.plan === 'edu') return;
+
+  const { error: updatedUserError } = await supabase
+    .from('profiles')
+    .update({
+      plan,
+      stripe_subscription_id: stripeSubscriptionId,
+      pro_plan_expiration_date: proPlanExpirationDate,
+    })
+    .eq('id', userId);
+  if (updatedUserError) throw updatedUserError;
+  console.log(`User ${userId} updated to ${plan}`);
+}
+
+export async function updateUserAccountByEmail({
+  email,
+  plan,
+  stripeSubscriptionId,
+  proPlanExpirationDate,
+}: {
+  email: string;
+  plan?: string;
+  stripeSubscriptionId?: string;
+  proPlanExpirationDate?: Date;
+}) {
+  const { data: userProfile } = await supabase
+    .from('profiles')
+    .select('plan')
+    .eq('email', email)
+    .single();
+
+  if (userProfile?.plan === 'edu') return;
+
+  const { error: updatedUserError } = await supabase
+    .from('profiles')
+    .update({
+      plan,
+      stripe_subscription_id: stripeSubscriptionId,
+      pro_plan_expiration_date: proPlanExpirationDate,
+    })
+    .eq('email', email);
+  if (updatedUserError) throw updatedUserError;
+  console.log(`User ${email} updated to ${plan}`);
+}
+
+export async function downgradeUserAccount({
+  email,
+  stripeSubscriptionId,
+}: {
+  email?: string;
+  stripeSubscriptionId?: string;
+}) {
+  if (!email && !stripeSubscriptionId) {
+    throw new Error('Either email or stripeSubscriptionId must be provided');
+  }
+
+  const { data: userProfile } = await supabase
+    .from('profiles')
+    .select('plan')
+    .eq(
+      stripeSubscriptionId ? 'stripe_subscription_id' : 'email',
+      stripeSubscriptionId || email,
+    )
+    .single();
+
+  if (userProfile?.plan === 'edu') return;
+
+  const { error: updatedUserError } = await supabase
+    .from('profiles')
+    .update({
+      plan: 'free',
+    })
+    .eq(
+      stripeSubscriptionId ? 'stripe_subscription_id' : 'email',
+      stripeSubscriptionId || email,
+    );
+  if (updatedUserError) throw updatedUserError;
+  console.log(`User ${email || stripeSubscriptionId} downgraded to free plan`);
+}
+
+export async function extendMembershipByStripeSubscriptionId({
+  stripeSubscriptionId,
+  proPlanExpirationDate,
+}: {
+  stripeSubscriptionId: string;
+  proPlanExpirationDate: Date;
+}) {
+  // Extend pro / ultra plan
+  const { data: userProfile } = await supabase
+    .from('profiles')
+    .select('plan, email')
+    .eq('stripe_subscription_id', stripeSubscriptionId)
+    .single();
+
+  if (userProfile?.plan === 'edu') return;
+
+  const { error: updatedUserError } = await supabase
+    .from('profiles')
+    .update({
+      pro_plan_expiration_date: proPlanExpirationDate,
+    })
+    .eq('stripe_subscription_id', stripeSubscriptionId);
+  if (updatedUserError) throw updatedUserError;
+  console.log(
+    `User ${userProfile?.email} with plan ${userProfile?.plan} extended to ${proPlanExpirationDate}`,
+  );
 }
