492de314485f1f119c2a62d71fb4430223e30c91
1orzero <laokameng.6@gmail.com>
2024-06-05 10:06:16 +0800

    refactor: Update mjQueueService to use UUID for job identification

--- utils/server/mjQueueService.ts
+++ utils/server/mjQueueService.ts
@@ -1,76 +1,81 @@
 import redis from './upstashRedisClient';
 
+import { v4 as uuidv4 } from 'uuid';
+
 const QUEUE_KEY = 'priority_queue';
 const QUEUE_INFO_KEY = 'queue_info';
 
-interface QueueUser {
-  userId: string;
-  status: string;
+type JobStatus = 'QUEUED' | 'PROCESSING' | 'COMPLETED' | 'FAILED';
+
+interface BasedMjJob {
+  jobId: string;
+  status: JobStatus;
   enqueuedAt: string;
+}
+interface QueuedMjJob extends BasedMjJob {
+  status: 'QUEUED';
   position: number;
 }
+interface ProcessingMjJob extends BasedMjJob {
+  status: 'PROCESSING';
+  progress: number;
+}
+interface CompletedMjJob extends BasedMjJob {
+  status: 'COMPLETED';
+  imageUrl: string;
+}
+interface FailedMjJob extends BasedMjJob {
+  status: 'FAILED';
+  reason: string;
+}
+type MjJob = QueuedMjJob | ProcessingMjJob | CompletedMjJob | FailedMjJob;
 
-export async function addUserToQueue(userId: string, status: string) {
+export async function addJobToQueue(): Promise<QueuedMjJob['jobId']> {
   const enqueuedAt = new Date().toISOString();
-  const userInfo = { userId, status, enqueuedAt };
+  const jobId = uuidv4();
 
-  // Add user to the end of the list
-  await redis.rpush(QUEUE_KEY, userId);
+  await redis.rpush(QUEUE_KEY, jobId);
 
-  // Store user information in a hash
-  await redis.hset(`${QUEUE_INFO_KEY}:${userId}`, {
-    userId,
-    status,
+  await redis.hset(`${QUEUE_INFO_KEY}:${jobId}`, {
+    jobId,
+    status: 'QUEUED',
     enqueuedAt,
   });
-  console.log(`added ${userId} to queue`);
-  return userInfo;
+
+  console.log(`added jobId ${jobId} to queue`);
+  return jobId;
 }
 
-export async function getUserInfo(userId: string): Promise<QueueUser | null> {
-  // Retrieve the entire list to find the user's position
-  const userList = await redis.lrange(QUEUE_KEY, 0, -1);
-  const position = userList.indexOf(userId);
+export async function getJob(jobId: string): Promise<MjJob | null> {
+  const queueList = await redis.lrange(QUEUE_KEY, 0, -1);
+  const position = queueList.indexOf(jobId);
   console.log({
+    jobId,
     position,
-    userId,
   });
-  if (position === -1) return null; // User not found in the queue
+  if (position === -1) return null;
 
-  // Get user information from the hash
-  const userInfo = await redis.hgetall(`${QUEUE_INFO_KEY}:${userId}`);
+  const jobInfo = await redis.hgetall(`${QUEUE_INFO_KEY}:${jobId}`);
   console.log({
-    userInfo,
+    jobInfo,
   });
-  if (!userInfo) return null;
-
-  return {
-    userId,
-    position: position + 1, // Convert 0-based index to 1-based position
-    enqueuedAt: userInfo.enqueuedAt as string,
-    status: userInfo.status as string,
-  };
+  if (!jobInfo) return null;
+  return jobInfo as unknown as MjJob;
 }
 
-export async function processNextUser() {
-  const userId = await redis.lpop(QUEUE_KEY); // Pop from the beginning
-  console.log({
-    userId,
-  });
-  if (userId) {
-    // Process the user with userId
-    console.log(`Processing user ${userId}`);
+export async function processNextJob() {
+  const jobId = await redis.lpop(QUEUE_KEY); // Pop from the beginning
+  if (jobId) {
+    console.log(`Processing jobId ${jobId}`);
 
-    // Set status to 'processing'
-    await redis.hset(`${QUEUE_INFO_KEY}:${userId}`, { status: 'processing' });
+    await redis.hset(`${QUEUE_INFO_KEY}:${jobId}`, { status: 'PROCESSING' });
 
-    // Sleep for 10 seconds
+    // TODO: Sleep for 10 seconds, replace to callback instead
     await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    console.log(`User ${userId} processed, Now removing from queue`);
+    console.log(`Job ${jobId} processed, Now removing from queue`);
 
-    // Remove user information from the hash
-    await redis.del(`${QUEUE_INFO_KEY}:${userId}`);
-    console.log(`User ${userId} removed from queue`);
+    await redis.del(`${QUEUE_INFO_KEY}:${jobId}`);
+    console.log(`Job ${jobId} removed from queue`);
   }
 }
