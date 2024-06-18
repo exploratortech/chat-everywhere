163794527277533ae2dd56f938ea253b488c3efa
1orzero <laokameng.6@gmail.com>
2024-06-18 12:31:17 +0800

    refactor: to the production version of imageGen

--- utils/server/imageGen.ts
+++ utils/server/imageGen.ts
@@ -1,46 +1,17 @@
 import { FunctionCall } from '@/types/chat';
-import { OpenAIModels } from '@/types/openai';
 
 import { OPENAI_API_HOST } from '../app/const';
 
-export const translateAndEnhancePrompt = async (prompt: string) => {
-  const [openAIEndpoints, openAIKeys] = getEndpointsAndKeys(true);
-  const openAIEndpoint = openAIEndpoints[0] || '';
-  const openAIKey = openAIKeys[0] || '';
-
-  let url = `${openAIEndpoint}/openai/deployments/${process.env.AZURE_OPENAI_GPT_4_MODEL_NAME}/chat/completions?api-version=2024-02-01`;
-
-  const translateSystemPrompt = `
-    Act as an AI image generation expert and a professional translator, follow user's instruction as strictly as possible.
-    First determine the prompt's language:
-      If the prompt is not in English:
-        1. Translate the prompt to English, keeping in mind the context for simplicity
-        2. Embellish the translated prompt with additional details for enhanced visual appeal
-        3. Make sure your response is solely in English
-
-      if the prompt is in english:
-        1. Simply echo the original prompt verbatim
-        2. Refrain from making any alterations to the original prompt
-
-    Afterward, called the available function based on the following condition:
-      if the prompt is ready to be processed:
-        - call the 'generate-image' function with the prompt.
-      if there are anything wrong or the prompt is not ready for processing:
-        - call the 'error-message' function indicating the errorMessage
-
-    Important to note: you should always called the function with the name 'generate-image' or 'error-message' and there is no need to response without calling the function.
-    `;
-  const functionCallsToSend: FunctionCall[] = [
-    {
-      name: 'error-message',
-      description: 'error message',
-      parameters: {
-        type: 'object',
-        properties: {
-          errorMessage: {
-            type: 'string',
-            description: 'error message',
-          },
+const functionCallsToSend: FunctionCall[] = [
+  {
+    name: 'error-message',
+    description: 'error message',
+    parameters: {
+      type: 'object',
+      properties: {
+        errorMessage: {
+          type: 'string',
+          description: 'error message',
         },
       },
     },
@@ -60,7 +31,12 @@ export const translateAndEnhancePrompt = async (prompt: string) => {
   },
 ];
 
-  const completionResponse = await fetch(url, {
+async function callOpenAIAPI(
+  prompt: string,
+  functionCallsToSend: FunctionCall[],
+) {
+  const url = `${OPENAI_API_HOST}/v1/chat/completions`;
+  const response = await fetch(url, {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${process.env.OPENAI_API_GPT_4_KEY}`,
