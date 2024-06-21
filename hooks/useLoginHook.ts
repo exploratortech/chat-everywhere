a08f17206f35bfc5c2517dab072c5976f54ac399
1orzero <laokameng.6@gmail.com>
2024-06-21 16:06:22 +0800

    refacotr: add useLoginHook

--- hooks/useLoginHook.ts
+++ hooks/useLoginHook.ts
@@ -1,141 +1,151 @@
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
-import { useContext, useEffect } from 'react';
+import { Session, useSupabaseClient } from '@supabase/auth-helpers-react';
+import { Dispatch, useEffect } from 'react';
 import { toast } from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 
 import { isFeatureEnabled, updateUserInfo } from '@/utils/app/eventTracking';
+import { userProfileQuery } from '@/utils/server/supabase';
 
-import HomeContext from '@/components/home/home.context';
+import { User } from '@/types/user';
+
+import { HomeInitialState } from '@/components/home/home.state';
 
 import useTeacherPromptForStudent from './teacherPortal/useTeacherPromptForStudent';
 import useTeacherSettingsForStudent from './teacherPortal/useTeacherSettingsForStudent';
-import useUserProfile from './useUserProfile';
+import { ActionType } from './useCreateReducer';
 
-const useLoginHook = () => {
+const useLoginHook = (
+  user: User | null,
+  dispatch: Dispatch<ActionType<HomeInitialState>>,
+) => {
   const { t } = useTranslation('chat');
-  const {
-    state: { user },
-    dispatch,
-  } = useContext(HomeContext);
-  const session = useSession();
+
   const supabase = useSupabaseClient();
-  const { refetch: fetchUserProfile } = useUserProfile({
-    userId: session?.user.id,
-  });
   const { refetch: fetchTeacherPrompts } = useTeacherPromptForStudent();
   const { refetch: fetchTeacherSettings } = useTeacherSettingsForStudent();
 
+  const handleUserProfileUpdate = (session: Session) => {
+    userProfileQuery({ client: supabase, userId: session.user.id })
+      .then((userProfile) => {
+        if (!userProfile) return;
+        dispatch({ field: 'showLoginSignUpModel', value: false });
+        dispatch({ field: 'showOneTimeCodeLoginModel', value: false });
+        dispatch({
+          field: 'isPaidUser',
+          value: userProfile.plan !== 'free',
+        });
+        dispatch({ field: 'isTempUser', value: userProfile.isTempUser });
+        if (userProfile.isTempUser || userProfile.isTeacherAccount) {
+          fetchTeacherPrompts().then((res) => {
+            if (res.data) {
+              dispatch({
+                field: 'teacherPrompts',
+                value: res.data.prompts,
+              });
+            }
+          });
+        }
+        if (userProfile.isTempUser) {
+          fetchTeacherSettings().then((res) => {
+            if (res.data) {
+              dispatch({
+                field: 'teacherSettings',
+                value: res.data.settings,
+              });
+            }
+          });
+        }
+        dispatch({
+          field: 'isTeacherAccount',
+          value: userProfile.isTeacherAccount,
+        });
+        dispatch({
+          field: 'isUltraUser',
+          value: userProfile.plan === 'ultra',
+        });
+        dispatch({
+          field: 'hasMqttConnection',
+          value: userProfile.hasMqttConnection,
+        });
+        dispatch({
+          field: 'isConnectedWithLine',
+          value: userProfile.isConnectedWithLine,
+        });
+        dispatch({
+          field: 'user',
+          value: {
+            id: session.user.id,
+            email: session.user.email,
+            plan: userProfile.plan || 'free',
+            token: session.access_token,
+            referralCode: userProfile.referralCode,
+            referralCodeExpirationDate: userProfile.referralCodeExpirationDate,
+            proPlanExpirationDate: userProfile.proPlanExpirationDate,
+            hasReferrer: userProfile.hasReferrer,
+            hasReferee: userProfile.hasReferee,
+            isInReferralTrial: userProfile.isInReferralTrial,
+          },
+        });
+
+        dispatch({
+          field: 'featureFlags',
+          value: {
+            'enable-conversation-mode':
+              isFeatureEnabled('enable-conversation-mode') ||
+              process.env.NEXT_PUBLIC_ENV !== 'production',
+          },
+        });
+        updateUserInfo({
+          id: userProfile.id,
+          email: userProfile.email,
+          plan: userProfile.plan || 'free',
+          associatedTeacherId: userProfile.associatedTeacherId,
+          isTeacherAccount: userProfile.isTeacherAccount,
+          isTempUser: userProfile.isTempUser,
+          tempUserUniqueId: userProfile.tempUserUniqueId,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error(
+          t('Unable to load your information, please try again later.'),
+        );
+      })
+      .finally(() => {
+        dispatch({ field: 'appInitialized', value: true });
+        console.log('userProfileQuery appInitialized', true);
+      });
+  };
+
+  const checkSurveyFilled = (session: Session) => {
+    supabase
+      .from('user_survey')
+      .select('name')
+      .eq('uid', session.user.id)
+      .then(({ data }) => {
+        if (!data || data.length === 0) {
+          dispatch({ field: 'isSurveyFilled', value: false });
+        } else {
+          dispatch({ field: 'isSurveyFilled', value: true });
+        }
+      });
+  };
+
   useEffect(() => {
     const { data } = supabase.auth.onAuthStateChange((event, session) => {
-      console.log(event, session);
       if (event === 'INITIAL_SESSION') {
         // handle initial session
+        if (!session) {
+          dispatch({ field: 'appInitialized', value: true });
+        } else {
+          handleUserProfileUpdate(session);
+          checkSurveyFilled(session);
+        }
       } else if (event === 'SIGNED_IN') {
-        if (session?.user) {
+        if (session?.user && !user) {
           // User info has been updated for this session
-          if (session.user.id === user?.id) return;
-          fetchUserProfile()
-            .then((result) => {
-              const userProfile = result.data;
-              if (!userProfile) return;
-              dispatch({ field: 'showLoginSignUpModel', value: false });
-              dispatch({ field: 'showOneTimeCodeLoginModel', value: false });
-              dispatch({
-                field: 'isPaidUser',
-                value: userProfile.plan !== 'free',
-              });
-              dispatch({ field: 'isTempUser', value: userProfile.isTempUser });
-              if (userProfile.isTempUser || userProfile.isTeacherAccount) {
-                fetchTeacherPrompts().then((res) => {
-                  if (res.data) {
-                    dispatch({
-                      field: 'teacherPrompts',
-                      value: res.data.prompts,
-                    });
-                  }
-                });
-              }
-              if (userProfile.isTempUser) {
-                fetchTeacherSettings().then((res) => {
-                  if (res.data) {
-                    dispatch({
-                      field: 'teacherSettings',
-                      value: res.data.settings,
-                    });
-                  }
-                });
-              }
-              dispatch({
-                field: 'isTeacherAccount',
-                value: userProfile.isTeacherAccount,
-              });
-              dispatch({
-                field: 'isUltraUser',
-                value: userProfile.plan === 'ultra',
-              });
-              dispatch({
-                field: 'hasMqttConnection',
-                value: userProfile.hasMqttConnection,
-              });
-              dispatch({
-                field: 'isConnectedWithLine',
-                value: userProfile.isConnectedWithLine,
-              });
-              dispatch({
-                field: 'user',
-                value: {
-                  id: session.user.id,
-                  email: session.user.email,
-                  plan: userProfile.plan || 'free',
-                  token: session.access_token,
-                  referralCode: userProfile.referralCode,
-                  referralCodeExpirationDate:
-                    userProfile.referralCodeExpirationDate,
-                  proPlanExpirationDate: userProfile.proPlanExpirationDate,
-                  hasReferrer: userProfile.hasReferrer,
-                  hasReferee: userProfile.hasReferee,
-                  isInReferralTrial: userProfile.isInReferralTrial,
-                },
-              });
-
-              dispatch({
-                field: 'featureFlags',
-                value: {
-                  // Ignore feature flag on staging and local env
-                  'enable-conversation-mode':
-                    isFeatureEnabled('enable-conversation-mode') ||
-                    process.env.NEXT_PUBLIC_ENV !== 'production',
-                },
-              });
-              updateUserInfo({
-                id: userProfile.id,
-                email: userProfile.email,
-                plan: userProfile.plan || 'free',
-                associatedTeacherId: userProfile.associatedTeacherId,
-                isTeacherAccount: userProfile.isTeacherAccount,
-                isTempUser: userProfile.isTempUser,
-                tempUserUniqueId: userProfile.tempUserUniqueId,
-              });
-            })
-            .catch((error) => {
-              console.log(error);
-              toast.error(
-                t('Unable to load your information, please try again later.'),
-              );
-            });
-
-          //Check if survey is filled by logged in user
-          supabase
-            .from('user_survey')
-            .select('name')
-            .eq('uid', session.user.id)
-            .then(({ data }) => {
-              if (!data || data.length === 0) {
-                dispatch({ field: 'isSurveyFilled', value: false });
-              } else {
-                dispatch({ field: 'isSurveyFilled', value: true });
-              }
-            });
+          handleUserProfileUpdate(session);
+          checkSurveyFilled(session);
         }
       } else if (event === 'SIGNED_OUT') {
         // handle sign out event
@@ -151,118 +161,7 @@ const useLoginHook = () => {
     return () => {
       data.subscription.unsubscribe();
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  useEffect(() => {
-    console.log(session);
-    // if (session?.user) {
-    //   // User info has been updated for this session
-    //   if (session.user.id === user?.id) return;
-    //   fetchUserProfile()
-    //     .then((result) => {
-    //       const userProfile = result.data;
-    //       if (!userProfile) return;
-    //       dispatch({ field: 'showLoginSignUpModel', value: false });
-    //       dispatch({ field: 'showOneTimeCodeLoginModel', value: false });
-    //       dispatch({ field: 'isPaidUser', value: userProfile.plan !== 'free' });
-    //       dispatch({ field: 'isTempUser', value: userProfile.isTempUser });
-    //       if (userProfile.isTempUser || userProfile.isTeacherAccount) {
-    //         fetchTeacherPrompts().then((res) => {
-    //           if (res.data) {
-    //             dispatch({
-    //               field: 'teacherPrompts',
-    //               value: res.data.prompts,
-    //             });
-    //           }
-    //         });
-    //       }
-    //       if (userProfile.isTempUser) {
-    //         fetchTeacherSettings().then((res) => {
-    //           if (res.data) {
-    //             dispatch({
-    //               field: 'teacherSettings',
-    //               value: res.data.settings,
-    //             });
-    //           }
-    //         });
-    //       }
-    //       dispatch({
-    //         field: 'isTeacherAccount',
-    //         value: userProfile.isTeacherAccount,
-    //       });
-    //       dispatch({
-    //         field: 'isUltraUser',
-    //         value: userProfile.plan === 'ultra',
-    //       });
-    //       dispatch({
-    //         field: 'hasMqttConnection',
-    //         value: userProfile.hasMqttConnection,
-    //       });
-    //       dispatch({
-    //         field: 'isConnectedWithLine',
-    //         value: userProfile.isConnectedWithLine,
-    //       });
-    //       dispatch({
-    //         field: 'user',
-    //         value: {
-    //           id: session.user.id,
-    //           email: session.user.email,
-    //           plan: userProfile.plan || 'free',
-    //           token: session.access_token,
-    //           referralCode: userProfile.referralCode,
-    //           referralCodeExpirationDate:
-    //             userProfile.referralCodeExpirationDate,
-    //           proPlanExpirationDate: userProfile.proPlanExpirationDate,
-    //           hasReferrer: userProfile.hasReferrer,
-    //           hasReferee: userProfile.hasReferee,
-    //           isInReferralTrial: userProfile.isInReferralTrial,
-    //         },
-    //       });
-    //
-    //       dispatch({
-    //         field: 'featureFlags',
-    //         value: {
-    //           // Ignore feature flag on staging and local env
-    //           'enable-conversation-mode':
-    //             isFeatureEnabled('enable-conversation-mode') ||
-    //             process.env.NEXT_PUBLIC_ENV !== 'production',
-    //         },
-    //       });
-    //       updateUserInfo({
-    //         id: userProfile.id,
-    //         email: userProfile.email,
-    //         plan: userProfile.plan || 'free',
-    //         associatedTeacherId: userProfile.associatedTeacherId,
-    //         isTeacherAccount: userProfile.isTeacherAccount,
-    //         isTempUser: userProfile.isTempUser,
-    //         tempUserUniqueId: userProfile.tempUserUniqueId,
-    //       });
-    //     })
-    //     .catch((error) => {
-    //       console.log(error);
-    //       toast.error(
-    //         t('Unable to load your information, please try again later.'),
-    //       );
-    //     });
-    //
-    //   //Check if survey is filled by logged in user
-    //   supabase
-    //     .from('user_survey')
-    //     .select('name')
-    //     .eq('uid', session.user.id)
-    //     .then(({ data }) => {
-    //       if (!data || data.length === 0) {
-    //         dispatch({ field: 'isSurveyFilled', value: false });
-    //       } else {
-    //         dispatch({ field: 'isSurveyFilled', value: true });
-    //       }
-    //     });
-    // } else {
-    //   dispatch({
-    //     field: 'isSurveyFilled',
-    //     value: getIsSurveyFilledFromLocalStorage(),
-    //   });
-    // }
-  }, [session]);
 };
 export default useLoginHook;
