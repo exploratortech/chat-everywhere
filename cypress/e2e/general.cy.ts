baf85d144d3841b3d957c45cbbe0c5cef9d5ffdc
Jack Wu <jackwu833016@gmail.com>
2024-06-20 20:57:20 -0400

    Initial setup of Cypress

---
+++ cypress/e2e/general.cy.ts
@@ -0,0 +1,8 @@
+describe('Free user usage', () => {
+  it('is able to send messages', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('[data-cy="chat-input"]').type('Reply "Hello World" to me.')
+    cy.get('[data-cy="chat-send-button"]').click()
+    cy.get('[data-cy="assistant-respond-message"]', { timeout: 10000 }).should('contain.text', 'Hello World')
+  })
+})
