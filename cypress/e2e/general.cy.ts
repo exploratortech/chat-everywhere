3aa0d40da16d52c70baee7d4388315eaa9fb98cd
Jack Wu <jackwu833016@gmail.com>
2024-06-21 22:40:13 -0400

    Experimenting with Github action

--- cypress/e2e/general.cy.ts
+++ cypress/e2e/general.cy.ts
@@ -1,12 +1,22 @@
 describe('Free user usage', () => {
-  const hostUrl = process.env.CYPRESS_HOST_URL || "http://localhost:3000";
+  const hostUrl =
+    Cypress.env('CYPRESS_HOST_URL') ||
+    process.env.CYPRESS_HOST_URL ||
+    'http://localhost:3000';
 
   it('is able to send messages', () => {
-    cy.visit(hostUrl)
-    cy.get('[data-cy="chat-input"]').type('Reply "Hello World" to me.')
-    cy.get('[data-cy="chat-send-button"]').click()
-    cy.get('[data-cy="assistant-respond-message"]', { timeout: 10000 }).should('contain.text', 'Hello World')
-  })
-})
+    console.log('hostUrl: ', hostUrl);
+    console.log('CYPRESS_HOST_URL: ', Cypress.env('CYPRESS_HOST_URL'));
+    console.log('process.env.CYPRESS_HOST_URL: ', process.env.CYPRESS_HOST_URL);
+    
+    cy.visit(hostUrl);
+    cy.get('[data-cy="chat-input"]').type('Reply "Hello World" to me.');
+    cy.get('[data-cy="chat-send-button"]').click();
+    cy.get('[data-cy="assistant-respond-message"]', { timeout: 20000 }).should(
+      'contain.text',
+      'Hello World',
+    );
+  });
+});
 
-export {}
+export {};
