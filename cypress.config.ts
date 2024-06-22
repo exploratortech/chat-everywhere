82c1380fbde42bfe2f0b09ce14fcfccc23ece25d
Jack Wu <jackwu833016@gmail.com>
2024-06-21 23:03:12 -0400

    Experimenting with Github action

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,9 +4,7 @@ console.log('CYPRESS_HOST_URL: ', process.env.CYPRESS_HOST_URL);
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on: any) {
-      require('cypress-terminal-report/src/installLogsPrinter')(on);
-    },
+    setupNodeEvents() {},
     baseUrl: process.env.CYPRESS_HOST_URL || 'http://localhost:3000',
   },
 });
