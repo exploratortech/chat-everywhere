a5cceac1800726296b1df4436ed9a593c5f6373c
Jack Wu <jackwu833016@gmail.com>
2024-06-21 22:44:14 -0400

    Experimenting with Github action

--- cypress/e2e/general.cy.ts
+++ cypress/e2e/general.cy.ts
@@ -1,14 +1,14 @@
 describe('Free user usage', () => {
-  const hostUrl =
-    Cypress.env('CYPRESS_HOST_URL') ||
-    process.env.CYPRESS_HOST_URL ||
-    'http://localhost:3000';
+  const hostUrl = Cypress.env('CYPRESS_HOST_URL') || 'http://localhost:3000';
 
   it('is able to send messages', () => {
     console.log('hostUrl: ', hostUrl);
-    console.log('CYPRESS_HOST_URL: ', Cypress.env('CYPRESS_HOST_URL'));
+    console.log(
+      "Cypress.env('CYPRESS_HOST_URL'): ",
+      Cypress.env('CYPRESS_HOST_URL'),
+    );
     console.log('process.env.CYPRESS_HOST_URL: ', process.env.CYPRESS_HOST_URL);
-    
+
     cy.visit(hostUrl);
     cy.get('[data-cy="chat-input"]').type('Reply "Hello World" to me.');
     cy.get('[data-cy="chat-send-button"]').click();
