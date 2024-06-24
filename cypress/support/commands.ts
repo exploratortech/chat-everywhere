f2bec89325b5a5918352e46aacdc4e0901c5835a
1orzero <laokameng.6@gmail.com>
2024-06-24 12:44:22 +0800

    feat(cypress): add login custom command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -11,29 +11,23 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+Cypress.Commands.add('login', (email, password) => {
+  const hostUrl = Cypress.env('HOST_URL') || 'http://localhost:3000';
+  cy.visit(hostUrl);
+  // login
+  cy.get('[data-cy="sign-in-button"]').click();
+  cy.get('#email').type(email);
+  cy.get('#password').type(password);
+  cy.get('#auth-sign-in > :nth-child(1) > .supabase-auth-ui_ui-button').click();
+});
 
-export {}
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      login(email: string, password: string): Chainable<void>;
+    }
+  }
+}
+
+export {};
+
