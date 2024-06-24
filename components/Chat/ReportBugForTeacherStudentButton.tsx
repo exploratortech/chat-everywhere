b46cff3414c6de6a5fcbe33a18d85a35a850427f
1orzero <laokameng.6@gmail.com>
2024-06-24 14:47:49 +0800

    feat(Chat): add loading state to bug report button

--- components/Chat/ReportBugForTeacherStudentButton.tsx
+++ components/Chat/ReportBugForTeacherStudentButton.tsx
@@ -16,6 +16,7 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 
+import Spinner from '../Spinner';
 import HomeContext from '../home/home.context';
 import { Button } from '../ui/button';
 
@@ -31,9 +32,10 @@ const ReportBugForTeacherStudentButton: React.FC<Props> = ({
   } = useContext(HomeContext);
 
   const { t } = useTranslation('common');
+  const { t: modelT } = useTranslation('model');
   const [bugDescription, setBugDescription] = useState('');
 
-  const { mutateAsync: reportBug } = useReportBugMutation();
+  const { mutateAsync: reportBug, isLoading } = useReportBugMutation();
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     await reportBug({
@@ -70,7 +72,12 @@ const ReportBugForTeacherStudentButton: React.FC<Props> = ({
               rows={7}
             />
             <div className="flex justify-center pt-4">
-              <Button type="submit">Submit</Button>
+              <Button type="submit" disabled={isLoading}>
+                <div className="flex items-center gap-2">
+                  {isLoading && <Spinner />}
+                  {modelT('Submit')}
+                </div>
+              </Button>
             </div>
           </form>
         </DialogHeader>
