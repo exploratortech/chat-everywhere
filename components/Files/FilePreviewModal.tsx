6491f1827e1b611a8947a1ebdf689e6dcc87258a
1orzero <laokameng.6@gmail.com>
2024-07-03 17:36:43 +0800

    feat: Add FilePreviewModal component

---
+++ components/Files/FilePreviewModal.tsx
@@ -0,0 +1,50 @@
+import { UserFile } from '@/types/UserFile';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+import AudioPreview from './AudioPreview';
+import PDFPreview from './PDFPreview';
+import VideoPreview from './VideoPreview';
+
+interface FilePreviewModalProps {
+  file: UserFile;
+  children: React.ReactNode;
+}
+
+const FilePreviewModal: React.FC<FilePreviewModalProps> = ({
+  file,
+  children,
+}) => {
+  const renderPreview = () => {
+    if (file.filetype.startsWith('application/pdf')) {
+      return <PDFPreview url={file.objectPath} />;
+    } else if (file.filetype.startsWith('video/')) {
+      return <VideoPreview objectPath={file.objectPath} />;
+    } else if (file.filetype.startsWith('audio/')) {
+      return <AudioPreview url={file.objectPath} />;
+    } else {
+      return <p>Preview not available for this file type.</p>;
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger>{children}</DialogTrigger>
+      <DialogContent className="w-full min-w-[80dvw] min-h-[80dvh]">
+        <DialogHeader>
+          <DialogTitle>{file.filename}</DialogTitle>
+          <DialogDescription>{renderPreview()}</DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default FilePreviewModal;
