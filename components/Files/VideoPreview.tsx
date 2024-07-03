a8908d2ad55632b5525e99a0d5b8324784f98bfe
1orzero <laokameng.6@gmail.com>
2024-07-03 17:48:26 +0800

    chore: Add lazy loading for video previews

--- components/Files/VideoPreview.tsx
+++ components/Files/VideoPreview.tsx
@@ -1,22 +1,39 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import { useDownloadObjectUrl } from '@/hooks/file/useDownloadObjectUrl';
 
 const VideoPreview = ({ objectPath }: { objectPath: string }) => {
+  const [videoUrl, setVideoUrl] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
+
+  const { mutateAsync: downloadFile } = useDownloadObjectUrl();
+
+  useEffect(() => {
+    downloadFile(objectPath).then((res) => {
+      if (res.url) {
+        setVideoUrl(res.url);
+      } else {
+        setError('Failed to download video');
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (error) {
     return <div>Error: {error}</div>;
   }
 
   return (
-    <div className="video-preview">
-      {objectPath && (
+    <div className="max-w-[80dvw] max-h-[80dvh] w-full h-full flex items-center justify-center">
+      {videoUrl && (
         <video
           controls
           width="100%"
           height="auto"
+          className="max-w-full max-h-full object-contain"
           onError={() => setError('Failed to load video')}
         >
-          <source src={objectPath} type="video/mp4" />
+          <source src={videoUrl} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       )}
