5b8061f7112ecde2f6ce0900991696c8858b8a38
1orzero <laokameng.6@gmail.com>
2024-07-10 10:51:30 +0800

    fix(tests): uncomment image generation test steps

--- cypress/e2e/image-generation.cy.ts
+++ cypress/e2e/image-generation.cy.ts
@@ -36,25 +36,24 @@ describe('Image generation', () => {
     cy.get('[data-cy="chat-enhanced-menu"]').scrollIntoView().should('be.visible');
     cy.get('[data-cy="chat-mode-selector"]').select('AI Image');
 
-    // cy.get('[data-cy="chat-send-button"]').click();
-    // cy.get('[data-cy="assistant-respond-message"]').within(() => {
-    //   cy.get('div#mj-image-static-v2', { timeout: 120000 }).should('exist');
-    //   cy.get('div#mj-image-static-v2').within(() => {
-    //     // Check if the download button is visible
-    //     cy.get('button[data-cy="mj-image-v2-download-button"]').should('exist');
+    cy.get('[data-cy="chat-send-button"]').click();
+    cy.get('[data-cy="assistant-respond-message"]').within(() => {
+      cy.get('div#mj-image-static-v2', { timeout: 120000 }).should('exist');
+      cy.get('div#mj-image-static-v2').within(() => {
+        // Check if the download button is visible
+        cy.get('button[data-cy="mj-image-v2-download-button"]').should('exist');
+
+        // Check if the first message contain buttons like "U1", "U2", "V1", "V2"
+        cy.get('div[data-cy="mj-image-v2-button-container"]').should('exist');
+        cy.get('div[data-cy="mj-image-v2-button-container"]').within(() => {
+          cy.contains('button', 'U1').should('exist');
+          cy.contains('button', 'U2').should('exist');
+          cy.contains('button', 'V1').should('exist');
+          cy.contains('button', 'V2').should('exist');
+        });
+      });
+    });
     //
-    //     // Check if the first message contain buttons like "U1", "U2", "V1", "V2"
-    //     cy.get('div[data-cy="mj-image-v2-button-container"]').should('exist');
-    //     cy.get('div[data-cy="mj-image-v2-button-container"]').within(() => {
-    //       cy.contains('button', 'U1').should('exist');
-    //       cy.contains('button', 'U2').should('exist');
-    //       cy.contains('button', 'V1').should('exist');
-    //       cy.contains('button', 'V2').should('exist');
-    //     });
-    //   });
-    //
-    //
-    // });
     // Check if the Remaining Credits are displayed
     cy.get('div[data-cy="credit-counter"]').should('exist');
 
